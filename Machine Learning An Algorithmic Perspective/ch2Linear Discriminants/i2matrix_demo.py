fb0b8098354232fbf7cb4f6257e3f56972b44b1c
greatabel <myreceiver2for2github@gmail.com>
2016-03-03 16:52:56 +0800

    where function demo

--- Machine Learning An Algorithmic Perspective/ch2Linear Discriminants/i2matrix_demo.py
+++ Machine Learning An Algorithmic Perspective/ch2Linear Discriminants/i2matrix_demo.py
@@ -7,3 +7,11 @@ print(a)
 print(b)
 print('-'*10)
 print(c)
+
+print(" 'where' function demo:")
+# http://ilanever.com/article/detail.do;jsessionid=C3D1AB96550C8CED231EB3B3B08FD26B?id=427
+x = array([1,2,3,4])
+y = array([10,20,30,40])
+condition = array([True,True,False,False])
+z = where(condition,x,y)
+print(x,"\n",y,"\n",z)
