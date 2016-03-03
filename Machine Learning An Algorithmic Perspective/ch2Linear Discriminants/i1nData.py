2de674b17bca3c7a1d73ae6b3413b5642cf68291
greatabel <myreceiver2for2github@gmail.com>
2016-03-03 15:41:06 +0800

    mutiple 2 lists

--- Machine Learning An Algorithmic Perspective/ch2Linear Discriminants/i1nData.py
+++ Machine Learning An Algorithmic Perspective/ch2Linear Discriminants/i1nData.py
@@ -1,14 +1,18 @@
-nData = [[1,2],[3,4]]
+# for data in range(nData):
+#     for n in range(N):
+#         activation[data][n] = 0
+#         for m in range(M+1):
+#             activation[data][n] += weight[m][n] * inputs[data][m]
 
+#         # now dicide wether the neuron fires or not
+#         if activation[data][n] > 0:
+#             activation[data][n] = 1
+#         else:
+#             activation[data][n] = 0
+a = [1,2,3]
+b = [4,5,6]
 
-for data in range(nData):
-    for n in range(N):
-        activation[data][n] = 0
-        for m in range(M+1):
-            activation[data][n] += weight[m][n] * inputs[data][m]
-
-        # now dicide wether the neuron fires or not
-        if activation[data][n] > 0:
-            activation[data][n] = 1
-        else:
-            activation[data][n] = 0
+print("a=", a)
+print("b=", b)
+c = [a*b for a,b in zip(a,b)]
+print(c)
