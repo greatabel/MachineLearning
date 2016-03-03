793b165c7323b9d7c619623579728e64fe6c283d
greatabel <myreceiver2for2github@gmail.com>
2016-03-03 17:19:57 +0800

    my first perceptron

---
+++ Machine Learning An Algorithmic Perspective/ch2Linear Discriminants/i3perceptron.py
@@ -0,0 +1,11 @@
+from numpy import *
+
+inputs = array([ [0,0],[0,1],[1,0],[1,1]  ])
+print('inputs=\n',inputs)
+
+targets = array([ [0],[1],[1],[1] ])
+print('targets=\n',targets)
+
+import pcn_logic_eg
+p = pcn_logic_eg.pcn(inputs, targets)
+p.pcntrain(inputs, targets, 0.25, 6)
