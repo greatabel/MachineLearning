5d795b5ca3fd139cf5471ddeb3fbc3c04daad550
greatabel <myreceiver2for2github@gmail.com>
2016-08-07 23:06:36 +0800

    reshape

--- Building Machine Learning System with Python/ch1Getting Started with Python Machine Learning/i1numpy.py
+++ Building Machine Learning System with Python/ch1Getting Started with Python Machine Learning/i1numpy.py
@@ -3,4 +3,7 @@ import numpy as np
 if __name__ == "__main__":
     print(np.version.full_version)
     a = np.array([0,1,2,3,4,5])
-    print(a,a.ndim, a.shape)
+    print('a=',a,'a.ndim=',a.ndim, a.shape)
+
+    b = a.reshape((3,2))
+    print('b=',b)
