7d8449ee4b4beea329cdb4b9b74137507a825377
greatabel <myreceiver2for2github@gmail.com>
2016-08-13 23:10:06 +0800

    use timeit in py script

--- SciPy and NumPy/ch2Numpy/i1NumPy_arrays.py
+++ SciPy and NumPy/ch2Numpy/i1NumPy_arrays.py
@@ -1,4 +1,4 @@
-import numpy as ntpath
+import numpy as np
 
 # create an array with 10^7 elements
 
@@ -11,4 +11,14 @@ def list_times(alist, scalar):
     for i, val in enumerate(alist):
         alist[i] = val*scalar
     return alist
+
+# http://stackoverflow.com/questions/10361206/how-to-run-an-ipython-magic-from-a-script-or-timing-a-python-script
+# 允许需要：ipython i1NumPy_arrays.py
+from IPython import get_ipython
+ipython = get_ipython()
+
+ipython.magic("timeit arr * 1.1") 
+
+ipython.magic("timeit list_times(larr, 1.1)") 
+
 
