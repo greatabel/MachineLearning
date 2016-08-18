695dd517238e892cd02abcf640039228d3f4e173
greatabel <myreceiver2for2github@gmail.com>
2016-08-18 15:45:05 +0800

    i6recording.py

--- SciPy and NumPy/ch2Numpy/i5record_array.py
+++ SciPy and NumPy/ch2Numpy/i5record_array.py
@@ -2,6 +2,13 @@ import numpy as np
 
 # Creating an array of zeros and defining column types 
 recarr = np.zeros((2,), dtype=('i4,f4,a10'))
+
 toadd = [(1,2.,'Hello'),(2,3.,"World")]
 recarr[:] = toadd
-print('recarr=', recarr)
+print('recarr=', recarr )
+
+recarrA = np.zeros((2,), dtype=('f2,f4,a5'))
+print('recarrA=', recarrA)
+recarrA[:] = [(1.111111,2.11111111111,'abcdefghijklmnopqrst'),(11.111111,22.11111111111,'1abcdefghijklmnopqrst')]
+print('recarrA=', recarrA )
+
---
+++ SciPy and NumPy/ch2Numpy/i6recording.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+# Creating an array of zeros and defining column types 
+recarr = np.zeros((2,), dtype=('i4,f4,a10'))
+print('recarr=', recarr)
+# Now creating the columns we want to put # in the recarray
+col1 = np.arange(2) + 10
+col2 = np.arange(2, dtype=np.float32) 
+col3 = ['Hello', 'World']
+
+print('col1,col2,col3=',col1,col2,col3)
+# Here we create a list of tuples that is # identical to the previous toadd list. 
+# toadd = zip(col1, col2, col3)
+# python3 not zip
+toadd = list(zip(col1, col2, col3))
+
+
+print('toadd=', toadd)
+# Assigning values to reca
+recarr[:] = toadd
+print('recarr=', recarr)
+# Assigning names to each column, which
+# are now by default called 'f0', 'f1', and 'f2'.
+recarr.dtype.names = ('Integers' , 'Floats', 'Strings')
+# If we want to access one of the columns by its name, we # can do the following.
+print("\nrecarr['Integers']=",recarr['Integers'])
+# array([1, 2], dtype=int32)
