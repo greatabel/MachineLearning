dc201157366f773ee55a0f4a8cc5f05ddca84ec6
greatabel <myreceiver2for2github@gmail.com>
2016-08-19 17:06:30 +0800

    i10read_and_write.py

---
+++ SciPy and NumPy/ch2Numpy/i10read_and_write.py
@@ -0,0 +1,23 @@
+from termcolor import colored,cprint
+
+import numpy as np
+import numpy.random as rand
+
+# Opening the text file with the 'r' option, # which only allows reading capability
+f = open('i10somefile.txt', 'r')
+# Parsing the file and splitting each line, # which creates a list where each element of # it is one line
+alist = f.readlines()
+cprint(alist, 'green', 'on_red')
+# Closing file 
+f.close()
+
+# After a few operations, we open a new text file
+# to write the data with the 'w' option. If there
+# was data already existing in the file, it will be overwritten.
+f = open('i10somefile.txt', 'w')
+# Writing data to file 
+newdata = "test"
+f.writelines(newdata)
+
+# Closing file 
+f.close()
