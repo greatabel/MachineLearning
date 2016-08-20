2a0c05e0da9b76d9b5b0dc320e912913c8225f30
greatabel <myreceiver2for2github@gmail.com>
2016-08-20 17:24:32 +0800

    i11get_file_direct_to_numpy.py numpy.savetxt()

--- SciPy and NumPy/ch2Numpy/i11get_file_direct_to_numpy.py
+++ SciPy and NumPy/ch2Numpy/i11get_file_direct_to_numpy.py
@@ -6,4 +6,13 @@ import numpy.random as rand
 arr = np.loadtxt("i10somefile.txt")
 print('arr=',arr)
 
+# http://stackoverflow.com/questions/16621351/how-to-use-python-numpy-savetxt-to-write-strings-and-float-number-to-an-ascii-fi
+names  = np.array(['NAME_1', 'NAME_2', 'NAME_3'])
+floats = np.array([ 0.1234 ,  0.5678 ,  0.9123 ])
 
+ab = np.zeros(names.size, dtype=[('var1', 'S6'), ('var2', float)])
+ab['var1'] = names
+ab['var2'] = floats
+print(colored( (names,'#',floats,'#',ab), 'red'))
+
+np.savetxt('i11test.txt', ab, fmt="%10s %10.3f")
