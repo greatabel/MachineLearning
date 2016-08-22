f3b6eaad831e05fb29288618a42bb8b33264f0f5
greatabel <myreceiver2for2github@gmail.com>
2016-08-22 18:13:48 +0800

    i1dynamic threshold.py

--- SciPy and NumPy/ch3SciPy/i20signal_image_processing.py
+++ SciPy and NumPy/ch3SciPy/i20signal_image_processing.py
@@ -1,6 +1,9 @@
 import numpy as np
 from scipy.misc import imread, imsave 
 from glob import glob
+
+# 我为osx 的python3.5 允许脚本需要： pip3.5 install Pillow
+
 # Getting the list of files in the directory 
 files = glob('i20space/*.JPG')
 print(files)
@@ -12,4 +15,4 @@ for i in range(1, len(files)):
     print(i)
     im1 += imread(files[i]).astype(np.float32)
 # Saving img 
-imsave('stacked_image.jpg', im1)
+imsave('i20space/stacked_image.jpg', im1)
