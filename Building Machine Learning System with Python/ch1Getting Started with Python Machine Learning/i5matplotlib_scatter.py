2e2250c4ea8a26d11419ef86896d4990acc9f4ba
greatabel <myreceiver2for2github@gmail.com>
2016-08-29 11:01:44 +0800

    i5matplotlib_scatter.py

--- Building Machine Learning System with Python/ch1Getting Started with Python Machine Learning/i5matplotlib_scatter.py
+++ Building Machine Learning System with Python/ch1Getting Started with Python Machine Learning/i5matplotlib_scatter.py
@@ -7,7 +7,7 @@ def main():
     y1 = [20,30,40,50]
     # s is size
     # http://matplotlib.org/api/pyplot_api.html#matplotlib.pyplot.scatter
-    matplotlib.pyplot.scatter(x, y, s=100,c='green')
+    matplotlib.pyplot.scatter(x, y, s=150,c=[0.1,0.2,0.3,0.4 ])
     matplotlib.pyplot.scatter(x, y1, s=10, c='red')
     matplotlib.pyplot.show()
 
