5cbfbd6065ba7bb421107c6a4605084fded2fcf4
greatabel <myreceiver2for2github@gmail.com>
2016-08-29 15:14:32 +0800

    i7starting_with_straight_line.py

---
+++ Building Machine Learning System with Python/ch1Getting Started with Python Machine Learning/i7starting_with_straight_line.py
@@ -0,0 +1,43 @@
+import os
+import scipy as sp
+from termcolor import colored,cprint
+import matplotlib.pyplot as plt
+
+sp.random.seed(3)
+
+def error(f, x,y):
+    return sp.sum( (f(x) - y)**2 )
+
+data = sp.genfromtxt("data/web_traffic.tsv", delimiter="\t")
+print(data[:3])
+print(data.shape)
+
+x = data[:,0]
+y = data[:,1]
+
+x = x[~sp.isnan(y)]
+y = y[~sp.isnan(y)]
+
+plt.scatter(x,y, s=10)
+plt.title("We traffic over the last month")
+plt.xlabel("Time")
+plt.ylabel("Hits/hour")
+plt.xticks([w*7*24 for w in range(10)],
+    ['week %i' % w for w in range(10)])
+plt.autoscale(tight=True)
+plt.grid(True, linestyle='-', color='0.75')
+# plt.show()
+
+# create and plot models
+fp1, res1, rank1, sv1, rcond1 = sp.polyfit(x, y, 1, full=True)
+print("Model parameters of fp1: %s" % fp1)
+print("Error of the model of fp1:", res1)
+f1 = sp.poly1d(fp1)
+print(colored('error=','red'),error(f1, x, y))
+
+fx = sp.linspace(0,x[-1], 1000) 
+# generate X-values for plotting
+plt.plot(fx, f1(fx), linewidth=4)
+plt.legend(["d=%i" % f1.order], loc="upper left")
+plt.show()
+
