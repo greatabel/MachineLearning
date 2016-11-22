dfb90b1ef5da14cfa047d9aa318c26eec02f759d
greatabel <myreceiver2for2github@gmail.com>
2016-11-22 22:24:49 +0800

    ch1 gaussian

---
+++ Machine Learning An Algorithmic Perspective/ch1Introduction/i1plotGaussian.py
@@ -0,0 +1,12 @@
+import pylab as pl
+import numpy as np
+
+
+gaussian = lambda x: 1/(np.sqrt(2*np.pi)*1.5)*np.exp(-(x-0)**2/(2*(1.5**2)))
+x = np.arange(-5, 5, 0.5)
+y = gaussian(x)
+
+print('x=', x, '\ny=', y)
+
+pl.plot(x, y, 'k', linewidth=3, marker='o')
+pl.show()
