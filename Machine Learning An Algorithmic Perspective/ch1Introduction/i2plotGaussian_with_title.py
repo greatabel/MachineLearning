6e3ffca5a22fb3dbdaae2cbaa6228c2dd3119d72
greatabel <myreceiver2for2github@gmail.com>
2016-11-22 22:34:46 +0800

    i2plotGaussian_with_title.py

--- Machine Learning An Algorithmic Perspective/ch1Introduction/i2plotGaussian_with_title.py
+++ Machine Learning An Algorithmic Perspective/ch1Introduction/i2plotGaussian_with_title.py
@@ -3,10 +3,14 @@ import numpy as np
 
 
 gaussian = lambda x: 1/(np.sqrt(2*np.pi)*1.5)*np.exp(-(x-0)**2/(2*(1.5**2)))
-x = np.arange(-10, 10, 0.5)
+x = np.arange(-5, 5, 0.5)
 y = gaussian(x)
 
-print('x=', x, '\ny=', y)
-
+# print('x=', x, '\ny=', y)
 pl.plot(x, y, 'k', linewidth=3, marker='o')
+pl.xlabel('XXX')
+pl.ylabel('Y(X)')
+pl.axis([-5,5,0,0.3])
+pl.title('Gaussian Function ( mean 0, standard deviation 1.5)')
+
 pl.show()
