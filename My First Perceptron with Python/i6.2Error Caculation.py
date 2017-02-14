d69c130afacc990e8507fcc7debb5fe48e682c51
greatabel <myreceiver2for2github@gmail.com>
2017-02-14 17:28:40 +0800

    error

--- My First Perceptron with Python/i6.2Error Caculation.py
+++ My First Perceptron with Python/i6.2Error Caculation.py
@@ -3,36 +3,39 @@ import random
 Out = []
 
 # Array: Memory weights
-Wgh = []
+Wgh = [0.0, 0.0]
 
 Inp = []
+
+Wgh[0] = random.uniform(-1.0, 1.0)
+Wgh[1] = random.uniform(-1.0, 1.0)
 
 # abel hard code:
 # Inp = [ [1,2],[10,20],[100, 200]
 #         ]
 for i in range(5):
     Inp.append([random.uniform(-10.0, 10.0), random.uniform(-10.0, 10.0)])
-    Wgh.append([random.uniform(-10.0, 10.0), random.uniform(-10.0, 10.0)])
+
     Out.append(random.choice([True, False]))
-print('Inp=', Inp)
+
+for i, elem in enumerate(Inp):
+    print('Inp[',i,']=', elem)
+print('Expected Out=', Out)
+print('Wgh=', Wgh)
 
 # constants of the rigid parameters
 Thr = 2.0
 
 
-print('\nWgh:', Wgh)
-Sum = 0
 for i, elem in enumerate(Inp):
-    print('\nindex=', i,Inp[i])
+    print('\n turn:', i,'Inp=',Inp[i])
     # Perceptron caculateion for the case
-    Sum += Inp[i][0] * Wgh[i][0] + Inp[i][1] * Wgh[i][1]
-    print('Sum=', Sum)
+    Sum = Inp[i][0] * Wgh[0] + Inp[i][1] * Wgh[1]
     
     if Sum > Thr:
         Axn = 1
     else:
         Axn = 0
-    print('Axn:', Axn)
 
     Err = Out[i] - Axn
-    print('Err=', Err)
+    print('Sum=', Sum, 'Axn:', Axn, 'Expected Out=', Out[i], 'Err=', Err)
