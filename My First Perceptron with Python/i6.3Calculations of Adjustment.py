d3699523f479cdba21723fa61544c8d5046609db
greatabel <myreceiver2for2github@gmail.com>
2017-02-14 16:50:19 +0800

    i6.3Calculations of Adjustment.py

---
+++ My First Perceptron with Python/i6.3Calculations of Adjustment.py
@@ -0,0 +1,40 @@
+import random
+
+Out = []
+
+# Array: Memory weights
+Wgh = [0.0, 0.0]
+
+Inp = []
+
+# abel hard code:
+# Inp = [ [1,2],[10,20],[100, 200]
+#         ]
+for i in range(5):
+    Inp.append([random.uniform(-10.0, 10.0), random.uniform(-10.0, 10.0)])
+    Out.append(random.choice([True, False]))
+print('Inp=', Inp)
+print('Out=', Out)
+
+# constants of the rigid parameters
+Thr = 2.0
+
+Wgh[0] = random.uniform(-1.0, 1.0)
+Wgh[1] = random.uniform(-1.0, 1.0)
+
+print('Wgh:', Wgh)
+
+for i, elem in enumerate(Inp):
+    print('\nindex=', i,Inp[i])
+    # Perceptron caculateion for the case
+    Sum = Inp[i][0] * Wgh[0] + Inp[i][1] * Wgh[1]
+    print('Sum=', Sum)
+    
+    if Sum > Thr:
+        Axn = 1
+    else:
+        Axn = 0
+    print('Axn:', Axn)
+
+    Err = Out[i] - Axn
+    print('Err=', Err)
