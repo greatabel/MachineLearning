d25dcf8c74bd8929f3e8a85e5153c5bab4ef36bb
greatabel <myreceiver2for2github@gmail.com>
2017-02-14 16:12:38 +0800

    wgt

---
+++ My First Perceptron with Python/i6.1Perceptron calculation.py
@@ -0,0 +1,13 @@
+import random
+# Array: Memory weights
+Wgt = [0.0, 0.0]
+
+Inp = []
+
+# constants of the rigid parameters
+Thr = 2.0
+
+Wgt[0] = random.uniform(-1.0, 1.0)
+Wgt[1] = random.uniform(-1.0, 1.0)
+
+print('Wgt:', Wgt)
