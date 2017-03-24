a54656c7880ef565390949758e3d2bbc0118965e
greatabel <myreceiver2for2github@gmail.com>
2017-03-24 17:10:10 +0800

    pprint

--- neural-networks-and-deep-learning/network.py
+++ neural-networks-and-deep-learning/network.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+import pprint
 
 
 class Network(object):
@@ -10,7 +11,15 @@ class Network(object):
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x)
                             for x,y in zip(sizes[:-1], sizes[1:])]
+        print("sizes[1:], sizes[:-1] = ",sizes[1:], sizes[:-1])
         print("\n self.num_layers = ", self.num_layers,
               "\n self.sizes = ", self.sizes,
-              "\n self.biases = ", self.biases,
-              "\n self.weights =", self.weights )
+              "\n typeof(biases)=", type(self.biases),
+              "\n typeof(weights)=", type(self.weights),
+             )
+
+        pp = pprint.PrettyPrinter(indent=4)
+        print('\nbiases=') 
+        pp.pprint(self.biases)
+        print('\nweights=') 
+        pp.pprint(self.weights)  
