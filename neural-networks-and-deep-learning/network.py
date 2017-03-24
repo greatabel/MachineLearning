fa34cc1786a731e4a0188aa073c9124eb501f39c
greatabel <myreceiver2for2github@gmail.com>
2017-03-24 17:26:25 +0800

    init a network

--- neural-networks-and-deep-learning/network.py
+++ neural-networks-and-deep-learning/network.py
@@ -11,7 +11,10 @@ class Network(object):
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x)
                             for x,y in zip(sizes[:-1], sizes[1:])]
-        print("sizes[1:], sizes[:-1] = ",sizes[1:], sizes[:-1])
+        for x, y in zip([2,3],[3,1]):
+            print("x=",x, "y=" , y)
+
+        print("sizes, sizes[1:], sizes[:-1] = ",sizes,sizes[1:], sizes[:-1])
         print("\n self.num_layers = ", self.num_layers,
               "\n self.sizes = ", self.sizes,
               "\n typeof(biases)=", type(self.biases),
