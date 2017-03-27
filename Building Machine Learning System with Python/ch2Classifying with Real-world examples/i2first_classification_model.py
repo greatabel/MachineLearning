8dba4b3ef2805149837670073c4e26cd5c7d3031
greatabel <myreceiver2for2github@gmail.com>
2017-03-27 16:49:58 +0800

    i2first_classification_model.py

---
+++ Building Machine Learning System with Python/ch2Classifying with Real-world examples/i2first_classification_model.py
@@ -0,0 +1,31 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+from sklearn.datasets import load_iris
+from termcolor import colored
+
+
+data = load_iris()
+print(colored('len(data)=', 'red'),len(data),'type(data)=',type(data))
+print('\n '*3, 'data=', data,'\n'*3)
+
+features = data.data
+feature_names = data.feature_names
+target = data.target
+target_names = data.target_names
+# setosa = 山鸢尾, versicolor = 变色秋海棠  virginica = 维尔吉妮卡
+
+# ------- common part -------
+labels = target_names[target]
+
+# The petal length is the feature at position 2
+plength = features[:, 2]
+
+# Build an array of booleans:
+is_setosa = (labels == 'setosa')
+
+max_setosa = plength[is_setosa].max()
+min_non_setosa = plength[~is_setosa].min()
+
+print('Maximum of setosa: {0}.'.format(max_setosa))
+print('Minimum of others: {0}.'.format(min_non_setosa))
