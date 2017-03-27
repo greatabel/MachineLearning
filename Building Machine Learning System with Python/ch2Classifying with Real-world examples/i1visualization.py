d2bb8bc98ef40dd1831d29f9c0ddf9a4efb74fdc
greatabel <myreceiver2for2github@gmail.com>
2017-03-27 15:51:53 +0800

    show 3 flowers

--- Building Machine Learning System with Python/ch2Classifying with Real-world examples/i1visualization.py
+++ Building Machine Learning System with Python/ch2Classifying with Real-world examples/i1visualization.py
@@ -9,4 +9,21 @@ features = data.data
 feature_names = data.feature_names
 target = data.target
 target_names = data.target_names
+# setosa = 山鸢尾, versicolor = 变色秋海棠  virginica = 维尔吉妮卡
 print(target_names)
+
+for t in range(3):
+    if t == 0:
+        c = 'r'
+        marker = '>'
+    elif t == 1:
+        c = 'g'
+        marker = 'o'
+    elif t == 2:
+        c = 'b'
+        marker = 'x'
+    plt.scatter(features[target == t, 0], 
+                features[target == t ,1],
+                marker=marker,
+                c=c)
+    # plt.show()
