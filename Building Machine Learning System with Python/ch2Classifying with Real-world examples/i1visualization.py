13c3b7c9c6f529f98a32e312b785c9ffc8cb9eca
greatabel <myreceiver2for2github@gmail.com>
2017-03-27 15:59:59 +0800

    print some iris data"

--- Building Machine Learning System with Python/ch2Classifying with Real-world examples/i1visualization.py
+++ Building Machine Learning System with Python/ch2Classifying with Real-world examples/i1visualization.py
@@ -3,14 +3,15 @@ import numpy as np
 
 from sklearn.datasets import load_iris
 data = load_iris()
-print('len(data)=',len(data))
+print('len(data)=',len(data),'type(data)=',type(data))
+print('\n '*3, 'data=', data,'\n'*3)
 
 features = data.data
 feature_names = data.feature_names
 target = data.target
 target_names = data.target_names
 # setosa = 山鸢尾, versicolor = 变色秋海棠  virginica = 维尔吉妮卡
-print(target_names)
+print('target_names=',target_names)
 
 for t in range(3):
     if t == 0:
