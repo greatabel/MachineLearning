18b3242647dfc98f050a88f6fd4c9e0586e90602
greatabel <myreceiver2for2github@gmail.com>
2017-03-27 16:32:19 +0800

    i0figure.py

--- Building Machine Learning System with Python/ch2Classifying with Real-world examples/i1visualization.py
+++ Building Machine Learning System with Python/ch2Classifying with Real-world examples/i1visualization.py
@@ -2,8 +2,11 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 from sklearn.datasets import load_iris
+from termcolor import colored
+
+
 data = load_iris()
-print('len(data)=',len(data),'type(data)=',type(data))
+print(colored('len(data)=', 'red'),len(data),'type(data)=',type(data))
 print('\n '*3, 'data=', data,'\n'*3)
 
 features = data.data
@@ -11,7 +14,14 @@ feature_names = data.feature_names
 target = data.target
 target_names = data.target_names
 # setosa = 山鸢尾, versicolor = 变色秋海棠  virginica = 维尔吉妮卡
-print('target_names=',target_names)
+print(colored('target_names=', 'red'),target_names)
+print(colored('len(target)=', 'red'),len(target))
+print(colored('len(features)=', 'blue'),len(features),'type(features)=',type(features))
+# print(colored('data.DESCR=', 'red'),data.DESCR)
+print(' - 萼片sepal length in cm\
+        - 萼片sepal width in cm\
+        - 花瓣petal length in cm\
+        - 花瓣petal width in cm')
 
 for t in range(3):
     if t == 0:
@@ -27,4 +37,5 @@ for t in range(3):
                 features[target == t ,1],
                 marker=marker,
                 c=c)
+
     # plt.show()
