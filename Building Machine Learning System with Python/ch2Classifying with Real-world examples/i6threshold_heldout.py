4cf2777c6248f548c4c6307de335143eaf59412b
greatabel <myreceiver2for2github@gmail.com>
2017-03-29 16:48:47 +0800

    filter

--- Building Machine Learning System with Python/ch2Classifying with Real-world examples/i6threshold_heldout.py
+++ Building Machine Learning System with Python/ch2Classifying with Real-world examples/i6threshold_heldout.py
@@ -1,5 +1,6 @@
 import numpy
 from sklearn.datasets import load_iris
+from termcolor import colored
 
 from i5threshold import fit_model, accuracy
 
@@ -9,5 +10,12 @@ labels = data['target_names'][data['target']]
 
 print('features:', features)
 print('labels:', labels)
+print(colored('='*20, 'red'))
 
+# we remove setosa examples as they are too easy:
+is_setosa = (labels == 'setosa')
+features = features[~is_setosa]
+labels = labels[~is_setosa]
 
+print('features:', features)
+print('labels:', labels)
