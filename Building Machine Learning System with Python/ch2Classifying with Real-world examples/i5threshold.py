0395b16d520602cd254da8ddf4809d6da6849307
greatabel <myreceiver2for2github@gmail.com>
2017-03-29 17:09:28 +0800

    model fit

--- Building Machine Learning System with Python/ch2Classifying with Real-world examples/i5threshold.py
+++ Building Machine Learning System with Python/ch2Classifying with Real-world examples/i5threshold.py
@@ -1,13 +1,36 @@
-import numpy
+import numpy as np
+from termcolor import colored
 
 def fit_model(features, labels):
     best_acc = -1.0
     for fi in range(features.shape[1]):
         thresh = features[:, fi].copy()
         # test all featur value in order
+        print(colored('-'*20, 'red'))
         print('before sort thresh:',thresh)
         thresh.sort()
         print('before sort thresh:',thresh)
+
+        for t in thresh:
+            pred = (features[:, fi] > t)
+
+            # measure accuray of this
+            accuracy = (pred == labels).mean()
+
+            rev_acc = (pred == ~labels).mean()
+
+            if rev_acc > accuracy:
+                accuracy = rev_acc
+                reverse = True
+            else:
+                reverse = False
+
+            if accuracy > best_acc:
+                best_acc = accuracy
+                best_fi = fi
+                best_t = t
+                best_reverse = reverse
+    return best_t, best_fi, best_reverse
 
 
 
--- Building Machine Learning System with Python/ch2Classifying with Real-world examples/i6threshold_heldout.py
+++ Building Machine Learning System with Python/ch2Classifying with Real-world examples/i6threshold_heldout.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 from sklearn.datasets import load_iris
 from termcolor import colored
 
@@ -8,7 +8,8 @@ data = load_iris()
 features = data['data']
 labels = data['target_names'][data['target']]
 
-print('features:', features)
+# print('features:', features)
+print('len(features)=',len(features))
 print('labels:', labels)
 print(colored('='*20, 'red'))
 
@@ -17,5 +18,22 @@ is_setosa = (labels == 'setosa')
 features = features[~is_setosa]
 labels = labels[~is_setosa]
 
-print('features:', features)
+# print('features:', features)
 print('labels:', labels)
+print('len(features)=',len(features))
+
+# Now we classify virginica vs non-virginica
+is_virginica = (labels == 'virginica')
+
+# Split the data in two: testing and training
+testing = np.tile([True, False], 50) # testing = [True,False,True,False,True,False...]
+# print('testing=',testing,len(testing))
+
+# Training is the negation of testing: i.e., datapoints not used for testing,
+# will be used for training
+training = ~testing
+# print('traing=', training,len(training))
+print('len(features[training]):',len(features[training]))
+
+model = fit_model(features[training], is_virginica[training])
+print('model:', model)
