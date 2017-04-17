faa4fe093a0e956a5a0b535661240d7bdb63219b
greatabel <myreceiver2for2github@gmail.com>
2017-04-17 15:19:08 +0800

    i8load.py

--- Building Machine Learning System with Python/ch2Classifying with Real-world examples/i9seeds_knn_sklearn.py
+++ Building Machine Learning System with Python/ch2Classifying with Real-world examples/i9seeds_knn_sklearn.py
@@ -1,3 +1,11 @@
 from i8load import load_dataset
+import numpy as np
+
+# Import sklearn implementation of KNN
+from sklearn.neighbors import KNeighborsClassifier
 
 features, labels = load_dataset('seeds')
+classifier = KNeighborsClassifier(n_neighbors=4)
+
+n = len(features)
+print('n = len(features) n =', n)
