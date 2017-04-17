eec958fe0a311097812f96171585486de0b5d0c3
greatabel <myreceiver2for2github@gmail.com>
2017-04-17 15:22:18 +0800

    i9seeds_knn_sklearn.py

--- Building Machine Learning System with Python/ch2Classifying with Real-world examples/i9seeds_knn_sklearn.py
+++ Building Machine Learning System with Python/ch2Classifying with Real-world examples/i9seeds_knn_sklearn.py
@@ -9,3 +9,11 @@ classifier = KNeighborsClassifier(n_neighbors=4)
 
 n = len(features)
 print('n = len(features) n =', n)
+
+correct = 0.0
+for ei in range(n):
+    training = np.ones(n, bool)
+    print('training=', training)
+    training[ei] = 0
+    testing = ~training
+    print('#training=', training)
