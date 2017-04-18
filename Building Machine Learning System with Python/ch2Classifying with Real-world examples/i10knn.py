9eb969ee585282463f786dc6f518166fbf9efd13
greatabel <myreceiver2for2github@gmail.com>
2017-04-18 14:57:14 +0800

    i10knn.py

--- Building Machine Learning System with Python/ch2Classifying with Real-world examples/i10knn.py
+++ Building Machine Learning System with Python/ch2Classifying with Real-world examples/i10knn.py
@@ -7,13 +7,37 @@ def fit_model(k, features, labels):
 def plurality(xs):
     from collections import defaultdict
     counts = defaultdict(int)
+    # print('counts', counts)
     for x in xs:
         counts[x] += 1
+        # print('counts['+ x +']=', counts[x])
     maxv = max(counts.values())
     for k, v in counts.items():
+        # print('k,v =',k, v)
         if v == maxv:
             return k
 
+# This function was called ``apply_model`` in the first edition
+def predict(model, features):
+    '''Apply k-nn model'''
+    k, train_feats, labels = model
+    results = []
+    for f in features:
+        label_dist = []
+        # Compute all distances:
+        for t, ell in zip(train_feats, labels):
+            label_dist.append((np.linalg.norm(f - t), ell))
+        label_dist.sort(key=lambda d_ell: d_ell[0])
+        label_dist = label_dist[:k]
+        results.append(plurality([ell for _, ell in label_dist]))
+
+    return np.array(results)
+
+
+def accuracy(features, labels, model):
+    preds = predict(model, features)
+    return np.mean(preds == labels)
+
 # if __name__ == "__main__":
-#     a = ['a','b','a','c']
+#     a = ['a','b','a','c','b','c','b']
 #     print(plurality(a))
