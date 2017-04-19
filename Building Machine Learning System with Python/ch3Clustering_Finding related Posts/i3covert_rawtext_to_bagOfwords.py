79a148aea9512cfb219813951eb96e8607f62b2e
greatabel <myreceiver2for2github@gmail.com>
2017-04-19 15:36:12 +0800

    new_post_vec.toarray()

--- Building Machine Learning System with Python/ch3Clustering_Finding related Posts/i3covert_rawtext_to_bagOfwords.py
+++ Building Machine Learning System with Python/ch3Clustering_Finding related Posts/i3covert_rawtext_to_bagOfwords.py
@@ -1,4 +1,6 @@
 from sklearn.feature_extraction.text import CountVectorizer
+from termcolor import colored
+import os
 
 vectorizer = CountVectorizer(min_df=1)
 print('dir(vectorizer)=',dir(vectorizer),'\nvectorizer=', vectorizer,'\n')
@@ -8,12 +10,23 @@ X = vectorizer.fit_transform(content)
 print('vectorizer.get_feature_names()=',vectorizer.get_feature_names())
 print(X.toarray().transpose())
 
+
 print(colored('*'*25, 'red'))
 
+
 from i2utils import DATA_DIR
+
+TOY_DIR = os.path.join(DATA_DIR, "toy")
 posts = [open(os.path.join(TOY_DIR, f)).read() for f in os.listdir(TOY_DIR)]
+print('posts:', posts)
 X_train = vectorizer.fit_transform(posts)
-print('vectorizer.get_feature_names()=',vectorizer.get_feature_names())
-print(X.toarray().transpose())
+
 num_samples, num_features = X_train.shape
 print("#samples: %d, #featues: %d" %(num_samples, num_features))
+print('vectorizer.get_feature_names()=',vectorizer.get_feature_names())
+
+# 向量化
+new_post = "imaging databases"
+new_post_vec = vectorizer.transform([new_post])
+print('new_post_vec =', new_post_vec)
+print('new_post_vec.toarray() =',new_post_vec.toarray())
