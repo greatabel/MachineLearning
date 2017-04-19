7ab8e4ce8995dfb9ad0557fc0d8beffc0fbc2228
greatabel <myreceiver2for2github@gmail.com>
2017-04-19 12:28:49 +0800

    i3covert_rawtext_to_bagOfwords.py

--- Building Machine Learning System with Python/ch3Clustering_Finding related Posts/i3covert_rawtext_to_bagOfwords.py
+++ Building Machine Learning System with Python/ch3Clustering_Finding related Posts/i3covert_rawtext_to_bagOfwords.py
@@ -1,4 +1,8 @@
 from sklearn.feature_extraction.text import CountVectorizer
 
 vectorizer = CountVectorizer(min_df=1)
-print('dir(vectorizer)=',dir(vectorizer))
+print('dir(vectorizer)=',dir(vectorizer),'\nvectorizer=', vectorizer,'\n')
+
+content = ["How to format my hard disk", "Hard disk format problems "]
+X = vectorizer.fit_transform(content)
+print('vectorizer.get_feature_names()=',vectorizer.get_feature_names())
