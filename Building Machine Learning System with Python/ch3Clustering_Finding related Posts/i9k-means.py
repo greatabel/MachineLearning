023caa48705ded3c57efafc3000438b1f9079f57
greatabel <myreceiver2for2github@gmail.com>
2017-04-21 16:25:14 +0800

    groups

--- Building Machine Learning System with Python/ch3Clustering_Finding related Posts/i9k-means.py
+++ Building Machine Learning System with Python/ch3Clustering_Finding related Posts/i9k-means.py
@@ -2,8 +2,21 @@
 
 import sklearn.datasets
 import scipy as sp
+from termcolor import colored
 
 print('sklearn.datasets.get_data_home()=', sklearn.datasets.get_data_home())
 
 all_data = sklearn.datasets.fetch_20newsgroups(subset="all")
 print("Number of total posts: %i" % len(all_data.filenames))
+print(all_data.filenames[0:3])
+
+print(colored('*'*25, 'magenta'),'target_names')
+print(all_data.target_names, '\n'*3)
+
+#  选取一些新闻组进行 训练
+groups = [
+    'comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardware',
+    'comp.sys.mac.hardware', 'comp.windows.x', 'sci.space']
+train_data = sklearn.datasets.fetch_20newsgroups(subset="train",
+                                                 categories=groups)
+print("Number of training posts in tech groups:", len(train_data.filenames))
