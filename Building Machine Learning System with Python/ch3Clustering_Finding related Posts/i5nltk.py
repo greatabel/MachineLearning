e591246bb8311497e0fdf1beedb5350dd7915ed3
greatabel <myreceiver2for2github@gmail.com>
2017-04-21 11:35:28 +0800

    i5nltk.py

--- Building Machine Learning System with Python/ch3Clustering_Finding related Posts/i5nltk.py
+++ Building Machine Learning System with Python/ch3Clustering_Finding related Posts/i5nltk.py
@@ -6,3 +6,10 @@ print(s.stem('imaging'))
 print(s.stem('image'))
 print(s.stem('imagination'))
 print(s.stem('imagine'))
+
+from termcolor import colored
+print(colored('*'*25, 'red'))
+print('----verb:----')
+print(s.stem('buys'))
+print(s.stem('buying'))
+print(s.stem('bought'))
