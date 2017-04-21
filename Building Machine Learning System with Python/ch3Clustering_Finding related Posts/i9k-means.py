7ee61488de3d837b0b003d80b8ba2c068e792123
greatabel <myreceiver2for2github@gmail.com>
2017-04-21 16:17:34 +0800

    fetch 20newgroups

--- Building Machine Learning System with Python/ch3Clustering_Finding related Posts/i9k-means.py
+++ Building Machine Learning System with Python/ch3Clustering_Finding related Posts/i9k-means.py
@@ -4,3 +4,6 @@ import sklearn.datasets
 import scipy as sp
 
 print('sklearn.datasets.get_data_home()=', sklearn.datasets.get_data_home())
+
+all_data = sklearn.datasets.fetch_20newsgroups(subset="all")
+print("Number of total posts: %i" % len(all_data.filenames))
