d77b03cde198602a1fa355b6f8d302d8902fdce1
greatabel <myreceiver2for2github@gmail.com>
2017-04-21 11:33:55 +0800

    i5nltk.py

--- Building Machine Learning System with Python/ch3Clustering_Finding related Posts/i5nltk.py
+++ Building Machine Learning System with Python/ch3Clustering_Finding related Posts/i5nltk.py
@@ -1,4 +1,8 @@
 import nltk.stem
-
+# stemmer 词干分析器
 s = nltk.stem.SnowballStemmer('english')
 print(s.stem('graphics'))
+print(s.stem('imaging'))
+print(s.stem('image'))
+print(s.stem('imagination'))
+print(s.stem('imagine'))
