db34bfac4b82357cf2f571f391f5df17eb9d9785
greatabel <myreceiver2for2github@gmail.com>
2017-04-26 15:22:57 +0800

    run bash

--- Building Machine Learning System with Python/ch4Topic Modeling/i0Latent Dirichlet Allocation.py
+++ Building Machine Learning System with Python/ch4Topic Modeling/i0Latent Dirichlet Allocation.py
@@ -3,6 +3,12 @@
 from gensim import corpora, models, similarities
 
 NUM_TOPICS = 100
+
+# Check that data exists
+if not path.exists('./data/ap/ap.dat'):
+    print('Error: Expected data to be present at data/ap/')
+    print('Please cd into ./data & run ./download_ap.sh')
+    
 # Load the data
 corpus = corpora.BleiCorpus('./data/ap/ap.dat',
  './data/ap/vocab.txt')
