d982cfa7411dc376342867c38d7889bd638fa20b
greatabel <myreceiver2for2github@gmail.com>
2017-04-27 23:49:53 +0800

    i1for_understand_ap_datasets.py

--- Building Machine Learning System with Python/ch4Topic Modeling/i1for_understand_ap_datasets.py
+++ Building Machine Learning System with Python/ch4Topic Modeling/i1for_understand_ap_datasets.py
@@ -1,16 +1,21 @@
 import re
 
+#  https://github.com/blei-lab/lda-c/blob/master/readme.txt
+
 print('Better understand ./data/ap/ap.dat and ap.txt')
 with open("./data/ap/ap.txt") as f:
     contents = f.read()
-    count = sum(1 for x in re.finditer(r"<DOC>", contents))
-    print(count)
+    countA = sum(1 for x in re.finditer(r"<DOC>", contents))
+    print(countA)
 
 with open("./data/ap/ap.dat") as f:
     contents = f.read()
     countB = sum(1 for x in re.finditer(r"\n", contents))
     print(countB)
 
+if countA == countB:
+    print("1. 至少暗示了ap.txt和ap.dat的对应关系！")
+    
 ap_txt_9 = "Here is a summary of developments in forest and brush fires in Western states:"
 ap_dat_9 = "7 5829:1 4040:1 2891:1 14:1 1783:1 381:1 2693:1"
 print("ap.txt 第9项是:\n" + 
