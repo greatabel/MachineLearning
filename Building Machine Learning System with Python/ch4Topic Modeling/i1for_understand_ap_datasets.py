cd599cd62f9fe8f49f29feb5573da6e62c1380fa
greatabel <myreceiver2for2github@gmail.com>
2017-04-28 00:13:57 +0800

    show ap dataset meaing

--- Building Machine Learning System with Python/ch4Topic Modeling/i1for_understand_ap_datasets.py
+++ Building Machine Learning System with Python/ch4Topic Modeling/i1for_understand_ap_datasets.py
@@ -15,9 +15,10 @@ with open("./data/ap/ap.dat") as f:
     countB = sum(1 for x in re.finditer(r"\n", contents))
     print(countB)
 
+print(colored('*'*25, 'red'))
 if countA == countB:
     print("1. 至少暗示了ap.txt和ap.dat的1 <---> 1 对应关系！")
-print(colored('*'*25, 'red'))
+
 
 
 ap_txt_9 = "Here is a summary of developments in forest and brush fires in Western states:"
@@ -27,8 +28,20 @@ print("ap.txt 第9项是:\n" +
 print("ap.dat 第9项是:\n" +
         ap_dat_9)
 
+vocab = None
+with open("./data/ap/vocab.txt") as f:
+    vocab = f.readlines()
+vocab = [x.strip() for x in vocab] 
+print("len(vocab)=", len(vocab))
+
 ap_dic = {}
 for i in ap_dat_9.split()[1:]:
     i_split = i.split(':')
-    ap_dic[i_split[0]] = i_split[1]
-print(ap_dic)
+    ap_dic[int(i_split[0])] = int(i_split[1])
+for k,v in ap_dic.items():
+    print(vocab[k], end='\t')
+    ap_txt_9 = ap_txt_9.lower().replace(vocab[k], "")
+print("\nremain=", ap_txt_9)
+
+print(colored('*'*25, 'red'))
+print("2. 这说明了ap.dat里面只是vocab.txt词汇在ap.txt每一项中出现的总数和单个次数。")
