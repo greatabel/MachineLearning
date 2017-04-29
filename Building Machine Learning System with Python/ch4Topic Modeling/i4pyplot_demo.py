856efa8c4bb8c3476a8dd74716b6d76774edd880
greatabel <myreceiver2for2github@gmail.com>
2017-04-29 22:07:40 +0800

    show how hist() work

--- Building Machine Learning System with Python/ch4Topic Modeling/i4pyplot_demo.py
+++ Building Machine Learning System with Python/ch4Topic Modeling/i4pyplot_demo.py
@@ -5,3 +5,10 @@ import matplotlib.pyplot as plt
 # y = np.sin(x)
 # plt.plot(x, y)
 # plt.show()
+num_topics_used = [10,20,20,30,30,30,40,40,40,40]
+fig,ax = plt.subplots()
+ax.hist(num_topics_used, np.arange(42))
+ax.set_ylabel('Nr of documents')
+ax.set_xlabel('Nr of topics')
+fig.tight_layout()
+plt.show()
