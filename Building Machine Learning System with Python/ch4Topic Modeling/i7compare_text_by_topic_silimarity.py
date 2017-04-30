d30f908e08e6e6743d446c7c597762e15890f547
greatabel <myreceiver2for2github@gmail.com>
2017-04-30 11:24:21 +0800

    text to words vector

--- Building Machine Learning System with Python/ch4Topic Modeling/i7compare_text_by_topic_silimarity.py
+++ Building Machine Learning System with Python/ch4Topic Modeling/i7compare_text_by_topic_silimarity.py
@@ -11,7 +11,8 @@ from gensim import corpora, models
 import sklearn.datasets
 import nltk.stem
 from collections import defaultdict
-
+from termcolor import colored
+import time
 #  如果发现 nltk.corpus.stopwords 找不到：
 # import nltk
 # nltk.download('stopwords')
@@ -20,6 +21,7 @@ english_stemmer = nltk.stem.SnowballStemmer('english')
 stopwords = set(nltk.corpus.stopwords.words('english'))
 stopwords.update(['from:', 'subject:', 'writes:', 'writes'])
 
+start = time.time()
 try:
     dataset = sklearn.datasets.load_mlcomp("20news-18828", "train",
                                        mlcomp_root='./data')
@@ -30,3 +32,25 @@ except:
     print("The website may need register a user.")
     print()
     raise
+
+end = time.time()
+print(colored('time:(end - start)=', 'magenta'), (end - start))
+# print(dir(dataset),dataset.DESCR, dataset.filenames, dataset.data[0:2],
+#     dataset.target[0:2], dataset.target_names[0:2])
+otexts = dataset.data
+texts = dataset.data
+# print('len(texts)=', len(texts),'#'*5, texts[0:2])
+texts = [t.decode('utf-8', 'ignore') for t in texts]
+# print('\nlen(texts)=', len(texts),'#'*5, texts[0:2])
+texts = [t.split() for t in texts]
+texts = [map(lambda w: w.lower(), t) for t in texts]
+texts = [filter(lambda s: not len(set("+-.?!()>@012345689") & set(s)), t)
+         for t in texts]
+texts = [filter(lambda s: (len(s) > 3) and (s not in stopwords), t)
+         for t in texts]
+texts = [map(english_stemmer.stem, t) for t in texts]
+# print('english_stemmer.stem=', help(english_stemmer.stem))
+# print('\nlen(texts)=', len(texts),'#'*5, texts[0:2])
+# for i in range(2):
+#     print(list(texts[i]))
+
