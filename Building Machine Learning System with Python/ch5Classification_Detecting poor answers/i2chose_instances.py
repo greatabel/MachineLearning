6ea13dc61c59efc49c7e4c9ab6679fd5ad6c26c6
greatabel <myreceiver2for2github@gmail.com>
2017-05-04 22:09:10 +0800

    misspelled_fraction

--- Building Machine Learning System with Python/ch5Classification_Detecting poor answers/i2chose_instances.py
+++ Building Machine Learning System with Python/ch5Classification_Detecting poor answers/i2chose_instances.py
@@ -5,6 +5,7 @@ except:
     import json
 import sys
 from collections import defaultdict
+from i0data import filtered, filtered_meta
 
 try:
     # http://stackoverflow.com/questions/3683181/cannot-install-pyenchant-on-osx
@@ -31,3 +32,18 @@ class EnchantMock:
 
 speller = EnchantMock()
 
+filter_meta = json.load(open(filtered_meta, "r"))
+
+def misspelled_fraction(p):
+    tokens = p.split()
+    if not tokens:
+        return 0.0
+    # step1 = sum(speller.check(t) for t in tokens)
+    # step2 = len(tokens)
+    # print('step: ',step1, step2, 1-float(step1)/step2)
+    return 1 - float(sum(speller.check(t) for t in tokens)) / len(tokens)
+
+
+if __name__ == "__main__":
+    result = misspelled_fraction("1.1 2.2 3.3")
+    print('result:', result)
