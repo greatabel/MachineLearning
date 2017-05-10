a282743f3a86a659112d00ef707734cf63f275e0
greatabel <myreceiver2for2github@gmail.com>
2017-05-11 00:06:49 +0800

    finish demo

--- Building Machine Learning System with Python/ch6Sentiment Analysis/i2classify_numpy_demo.py
+++ Building Machine Learning System with Python/ch6Sentiment Analysis/i2classify_numpy_demo.py
@@ -2,4 +2,20 @@ import numpy as np
 
 # 生成0，3 之间随机数 30个
 arr = np.random.randint(0, 3, 30)
-print(arr,type(arr))
+
+def map_to_sentiment(x):
+    if x == 0:
+        return "negative"
+    elif x == 1:
+        return "neutral"
+    elif x == 2:
+        return "positive"
+vfunc = np.vectorize(map_to_sentiment)
+sentiments = vfunc(arr)
+print(sentiments)
+
+classes = np.unique(sentiments)
+print('classes:', classes)
+
+for c in classes:
+    print("#%s: %i" %(c, sum(sentiments==c)))
