e01f434266279f1e73f15c3770d31836627dd101
greatabel <myreceiver2for2github@gmail.com>
2017-05-12 14:41:47 +0800

    group by

--- Building Machine Learning System with Python/ch6Sentiment Analysis/i5bayes.py
+++ Building Machine Learning System with Python/ch6Sentiment Analysis/i5bayes.py
@@ -9,4 +9,5 @@ from i4utils import load_sanders_data
 if __name__ == "__main__":
     X_orig, Y_orig = load_sanders_data()
     classes = np.unique(Y_orig)
-    print(classes)
+    for c in classes:
+        print("#%s: %i" % (c, sum(Y_orig == c)))
