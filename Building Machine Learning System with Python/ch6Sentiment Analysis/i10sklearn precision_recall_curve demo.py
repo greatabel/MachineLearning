5096cbba57e9878955a0e197ad2e993df616cc63
greatabel <myreceiver2for2github@gmail.com>
2017-05-16 21:46:14 +0800

    finally understand

--- Building Machine Learning System with Python/ch6Sentiment Analysis/i10sklearn precision_recall_curve demo.py
+++ Building Machine Learning System with Python/ch6Sentiment Analysis/i10sklearn precision_recall_curve demo.py
@@ -3,31 +3,37 @@ from sklearn.metrics import precision_recall_curve
 from termcolor import colored
 # http://scikit-learn.org/stable/modules/generated/sklearn.metrics.precision_recall_curve.html
 # https://www.zhihu.com/question/19645541
+# http://www.cnblogs.com/aquastone/p/random-classifier.html
+# http://stackoverflow.com/questions/38165273/sklearn-metrics-precision-recall-curve-why-are-the-precision-and-recall-returne
 
+# y_true = np.array([0, 0, 1, 1])
+# y_scores = np.array([0.1, 0.6, 0.3, 0.5])
+# precision, recall, thresholds = precision_recall_curve(
+#     y_true, y_scores)
 
-y_true = np.array([0, 0, 1, 1])
-y_scores = np.array([0.1, 0.6, 0.3, 0.5])
-precision, recall, thresholds = precision_recall_curve(
-    y_true, y_scores)
-
-print("precision=", precision) 
-print("recall=", recall)
-print("thresholds=", thresholds)
+# print("precision=", precision) 
+# print("recall=", recall)
+# print("thresholds=", thresholds)
 
 # demo 2:
 print( colored("<-> " * 15, 'red'))
 
 import numpy.random as r
 import pylab
-size = 10
+size = 10000
 y_true = np.array([ 1 if i >= 0.3 else 0 for i in r.random(size) ], dtype=np.float32)
 y_pred = r.random(size)
-print("y_true=", y_true)
-print("y_pred", y_pred)
+# y_pred1 = []
+# for i in range(size):
+#     y_pred1.append(0.1)
+# print('len(y_pred1)=', len(y_pred1))
+# print("y_true=", y_true)
+# print("y_pred", y_pred)
 precision, recall, th = precision_recall_curve(y_true, y_pred)
-print("recall=", recall)
-print("precision=", precision) 
-print("thresholds=", thresholds)
+# print("recall    (召回率：正例里你的预测覆盖了多少)=", recall)
+# print("precision (准确率:你的预测有多少是对的)    =", precision) 
+
+print("thresholds=", th)
 ax = pylab.subplot(2, 1, 2)
 ax.plot(recall, precision)
 ax.set_ylim([0.0, 1.0])
