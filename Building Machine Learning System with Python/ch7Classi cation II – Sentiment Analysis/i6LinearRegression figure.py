fe01b021000ee1e6c2feb8e1486a1b590660b928
greatabel <myreceiver2for2github@gmail.com>
2017-05-18 20:57:10 +0800

    plot I

--- Building Machine Learning System with Python/ch7Classi cation II – Sentiment Analysis/i6LinearRegression figure.py
+++ Building Machine Learning System with Python/ch7Classi cation II – Sentiment Analysis/i6LinearRegression figure.py
@@ -4,6 +4,9 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from matplotlib import pyplot as plt
 
+import warnings
+warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
+
 boston = load_boston()
 # boston = boston.data
 # print(boston[0:5], '#'*10, boston[:, 5][0:5])
@@ -19,3 +22,12 @@ x = boston.data[:, 5]
 # print(x)
 x = np.transpose(np.atleast_2d(x))
 # print(x)
+
+y = boston.target
+
+lr = LinearRegression(fit_intercept=False)
+lr.fit(x, y)
+
+ax.plot([0, boston.data[:, 5].max() + 1],
+         [0, lr.predict(boston.data[:, 5].max() + 1)], '-', lw=4)
+plt.show()
