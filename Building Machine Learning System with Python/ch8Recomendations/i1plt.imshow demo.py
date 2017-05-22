ef54379b3de097ba5a91bfed61d6b3a6015f6473
greatabel <myreceiver2for2github@gmail.com>
2017-05-22 18:01:22 +0800

    i1plt.imshow demo.py

---
+++ Building Machine Learning System with Python/ch8Recomendations/i1plt.imshow demo.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+# https://matplotlib.org/users/image_tutorial.html
+print('images -> numpy arrays')
+img = mpimg.imread('data/beatiful.png')
+print(img.shape)
+print('Plotting numpy arrays as images')
+imgplot = plt.imshow(img)
+# plt.show()
