124d1cb5ef2823834aebe03338208974e91ceace
greatabel <myreceiver2for2github@gmail.com>
2017-05-26 12:12:41 +0800

    i3combine two image.py

---
+++ Building Machine Learning System with Python/ch10Computer Vision/i3combine two image.py
@@ -0,0 +1,14 @@
+import numpy as np
+import mahotas as mh
+
+# This little script just builds an image with two examples, side-by-side:
+
+text = mh.imread("SimpleImageDataset/female1.png")
+building = mh.imread("SimpleImageDataset/male1.png")
+h, w, _ = text.shape
+print(h,w,'here')
+canvas = np.zeros((h, 2 * w + 128, 3), np.uint8)
+canvas[:, -w:] = building
+canvas[:, :w] = text
+canvas = canvas[::4, ::4]
+mh.imsave('data/figure10.png', canvas)
