1201af10fd03dba994ad459e85bc7515b7071013
greatabel <myreceiver2for2github@gmail.com>
2017-05-26 11:52:00 +0800

    i0read and show image.py

---
+++ Building Machine Learning System with Python/ch10Computer Vision/i0read and show image.py
@@ -0,0 +1,12 @@
+import mahotas as mh
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Load image & convert to B&W
+image = mh.imread('SimpleImageDataset/1.jpg')
+
+plt.imshow(image)
+# plt.show()
+
+image = image - image.mean()
+plt.show()
