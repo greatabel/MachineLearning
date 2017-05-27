16f37d259a60ecf08a4979127892269efda9fdce
greatabel <myreceiver2for2github@gmail.com>
2017-05-27 22:58:55 +0800

    i0Generalization vs. Rote Learning.py

--- Deep Learning with Python- A Hands-on Introduction/ch2Machine Learning Fundamentals/i0Generalization vs. Rote Learning.py
+++ Deep Learning with Python- A Hands-on Introduction/ch2Machine Learning Fundamentals/i0Generalization vs. Rote Learning.py
@@ -1,8 +1,16 @@
 import pylab
 import numpy
 
-x = numpy.linspace(-1, 1 , 10)
+x = numpy.linspace(-1, 1 , 100)
 signal = 2 + x + 2 * x * x
-noise = numpy.random.normal(0, 0.1, 10)
+noise = numpy.random.normal(0, 0.1, 100)
 y = signal + noise
-print('x=', x, 'y=', y)
+# print('x=', x, '\ny=', y)
+pylab.plot(signal, 'b')
+pylab.plot(y, 'g')
+pylab.plot(noise, 'r')
+pylab.xlabel("x")
+pylab.ylabel("y")
+pylab.legend(["Without Noise", "With Noise", "Noise"], loc = 2)
+
+pylab.show()
