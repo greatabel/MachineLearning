62066febbcca1fba4d5ad2df3ae46df7f66ae853
greatabel <myreceiver2for2github@gmail.com>
2017-05-29 16:07:46 +0800

    i1Model with degree.py

---
+++ Deep Learning with Python- A Hands-on Introduction/ch2Machine Learning Fundamentals/i1Model with degree.py
@@ -0,0 +1,32 @@
+import pylab
+import numpy
+
+x = numpy.linspace(-1, 1 , 100)
+signal = 2 + x + 2 * x * x
+noise = numpy.random.normal(0, 0.1, 100)
+y = signal + noise
+
+x_train = x[0:80]
+y_train = y[0:80]
+
+# model with degree 1
+pylab.figure()
+degree = 2
+
+X_train = numpy.column_stack([numpy.power(x_train,i) for i in range(0, degree)])
+model = numpy.dot(numpy.dot(numpy.linalg.inv(numpy.dot(X_train.transpose(),X_train)),
+    X_train.transpose()),y_train)
+pylab.plot(x,y,'g')
+pylab.xlabel("x")
+pylab.ylabel("y")
+predicted = numpy.dot(model, [numpy.power(x,i) for i in range(0,degree)])
+pylab.plot(x, predicted,'r')
+pylab.legend(["Actual", "Predicted"], loc = 2)
+train_rmse1 = numpy.sqrt(numpy.sum(numpy.dot(y[0:80] - predicted[0:80], y_train -
+    predicted[0:80])))
+test_rmse1 = numpy.sqrt(numpy.sum(numpy.dot(y[80:] - predicted[80:], y[80:] -
+    predicted[80:])))
+print("Train RMSE (Degree = 1)", train_rmse1)
+print("Test RMSE (Degree = 1)", test_rmse1)
+
+pylab.show()
