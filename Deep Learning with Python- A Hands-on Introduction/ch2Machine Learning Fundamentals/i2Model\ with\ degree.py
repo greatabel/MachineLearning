9e104c17c8e489c4264078c0734abbcda7f33c00
greatabel <myreceiver2for2github@gmail.com>
2017-05-29 17:11:19 +0800

    i1demoOf numpy.column_stack.py

--- Deep Learning with Python- A Hands-on Introduction/ch2Machine Learning Fundamentals/i2Model\ with\ degree.py
+++ Deep Learning with Python- A Hands-on Introduction/ch2Machine Learning Fundamentals/i2Model\ with\ degree.py
@@ -10,23 +10,27 @@ x_train = x[0:80]
 y_train = y[0:80]
 
 # model with degree 1
-pylab.figure()
-degree = 2
+def abel_model(degree):
+    pylab.figure()
+    # degree = 2
 
-X_train = numpy.column_stack([numpy.power(x_train,i) for i in range(0, degree)])
-model = numpy.dot(numpy.dot(numpy.linalg.inv(numpy.dot(X_train.transpose(),X_train)),
-    X_train.transpose()),y_train)
-pylab.plot(x,y,'g')
-pylab.xlabel("x")
-pylab.ylabel("y")
-predicted = numpy.dot(model, [numpy.power(x,i) for i in range(0,degree)])
-pylab.plot(x, predicted,'r')
-pylab.legend(["Actual", "Predicted"], loc = 2)
-train_rmse1 = numpy.sqrt(numpy.sum(numpy.dot(y[0:80] - predicted[0:80], y_train -
-    predicted[0:80])))
-test_rmse1 = numpy.sqrt(numpy.sum(numpy.dot(y[80:] - predicted[80:], y[80:] -
-    predicted[80:])))
-print("Train RMSE (Degree = 1)", train_rmse1)
-print("Test RMSE (Degree = 1)", test_rmse1)
+    X_train = numpy.column_stack([numpy.power(x_train,i) for i in range(0, degree)])
+    model = numpy.dot(numpy.dot(numpy.linalg.inv(numpy.dot(X_train.transpose(),X_train)),
+        X_train.transpose()),y_train)
+    pylab.plot(x,y,'g')
+    pylab.xlabel("x")
+    pylab.ylabel("y")
+    predicted = numpy.dot(model, [numpy.power(x,i) for i in range(0,degree)])
+    pylab.plot(x, predicted,'r')
+    pylab.legend(["Actual", "Predicted"], loc = 2)
+    train_rmse1 = numpy.sqrt(numpy.sum(numpy.dot(y[0:80] - predicted[0:80], y_train -
+        predicted[0:80])))
+    test_rmse1 = numpy.sqrt(numpy.sum(numpy.dot(y[80:] - predicted[80:], y[80:] -
+        predicted[80:])))
+    print("Train RMSE (Degree = 1)", train_rmse1)
+    print("Test RMSE (Degree = 1)", test_rmse1)
+
+abel_model(2)
+abel_model(3)
 
 pylab.show()
