3a951baf074c086d5d84d5e2aa410583f016ab3f
greatabel <myreceiver2for2github@gmail.com>
2017-05-30 08:37:45 +0800

    i3relationship of power and rmse.py

--- Deep Learning with Python- A Hands-on Introduction/ch2Machine Learning Fundamentals/i2Model\ with\ degree.py
+++ Deep Learning with Python- A Hands-on Introduction/ch2Machine Learning Fundamentals/i2Model\ with\ degree.py
@@ -17,6 +17,7 @@ def abel_model(degree):
     X_train = numpy.column_stack([numpy.power(x_train,i) for i in range(0, degree)])
     model = numpy.dot(numpy.dot(numpy.linalg.inv(numpy.dot(X_train.transpose(),X_train)),
         X_train.transpose()),y_train)
+    # print('model=', model,'type=', type(model),dir(model))
     pylab.plot(x,y,'g')
     pylab.xlabel("x")
     pylab.ylabel("y")
@@ -27,8 +28,8 @@ def abel_model(degree):
         predicted[0:80])))
     test_rmse1 = numpy.sqrt(numpy.sum(numpy.dot(y[80:] - predicted[80:], y[80:] -
         predicted[80:])))
-    print("Train RMSE (Degree = "+ str(degree - 1) +")", train_rmse1)
-    print("Test RMSE (Degree = " + str(degree - 1)+ ")", test_rmse1)
+    print("Train RMSE[标准差] (Degree = "+ str(degree - 1) +")", train_rmse1)
+    print("Test RMSE[标准差] (Degree = " + str(degree - 1)+ ")", test_rmse1)
 
 abel_model(2)
 abel_model(3)
