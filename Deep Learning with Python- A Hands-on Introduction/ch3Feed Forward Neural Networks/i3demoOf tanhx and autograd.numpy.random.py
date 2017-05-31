d69e3b01cebaefcb364b77ebe8076c77922fdbc6
greatabel <myreceiver2for2github@gmail.com>
2017-05-31 11:38:09 +0800

    i3demoOf tanhx and autograd.numpy.random.py

--- Deep Learning with Python- A Hands-on Introduction/ch3Feed Forward Neural Networks/i3demoOf tanhx and autograd.numpy.random.py
+++ Deep Learning with Python- A Hands-on Introduction/ch3Feed Forward Neural Networks/i3demoOf tanhx and autograd.numpy.random.py
@@ -1,5 +1,5 @@
 import autograd.numpy as np
-
+import autograd.numpy.random as npr
 #  https://zh.wikipedia.org/wiki/%E5%8F%8C%E6%9B%B2%E5%87%BD%E6%95%B0
 #  https://docs.scipy.org/doc/numpy/reference/generated/numpy.tanh.html
 
@@ -7,3 +7,9 @@ a = np.tanh((0, np.pi*1j, np.pi*1j/2))
 print(a,'#',len(a), '#',a[0])
 b = np.tanh((0,-1000000,1000000))
 print(b)
+
+print('autograd.numpy.random')
+n = npr.rand()
+print(n)
+n1 =npr.rand(10)
+print(n1)
