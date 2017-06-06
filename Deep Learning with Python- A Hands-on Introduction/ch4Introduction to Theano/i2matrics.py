7e47c756ea3d988cbbaa733ecc7c99e75515db74
greatabel <myreceiver2for2github@gmail.com>
2017-06-06 17:43:17 +0800

    i2matrics.py

---
+++ Deep Learning with Python- A Hands-on Introduction/ch4Introduction to Theano/i2matrics.py
@@ -0,0 +1,32 @@
+import numpy as np
+import theano.tensor as T
+from theano import function
+
+a = T.dmatrix('a')
+b = T.dmatrix('b')
+c = T.dmatrix('c')
+d = T.dmatrix('d')
+
+e = (a + b - c) * d
+
+f = function([a, b, c, d], e)
+
+a_data = np.array([
+    [1,1],
+    [1,1]
+    ])
+b_data = np.array([
+    [2,2],
+    [2,2]
+    ])
+c_data = np.array([
+    [5,5],
+    [5,5]
+    ])
+d_data = np.array([
+    [3,3],
+    [3,3]
+    ])
+
+print("Expected:", (a_data + b_data - c_data) * d_data)
+print("Via Theano:", f(a_data, b_data, c_data, d_data))
