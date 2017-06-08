cc23a190c3bc7f8b91b6617063ad6c132fae9fa3
greatabel <myreceiver2for2github@gmail.com>
2017-06-08 17:08:00 +0800

    i3functions with scalars and vectors.py

--- Deep Learning with Python- A Hands-on Introduction/ch4Introduction to Theano/i2matrics.py
+++ Deep Learning with Python- A Hands-on Introduction/ch4Introduction to Theano/i2matrics.py
@@ -2,6 +2,8 @@ import numpy as np
 import theano.tensor as T
 from theano import function
 
+from termcolor import colored
+
 a = T.dmatrix('a')
 b = T.dmatrix('b')
 c = T.dmatrix('c')
@@ -29,4 +31,5 @@ d_data = np.array([
     ])
 
 print("Expected:", (a_data + b_data - c_data) * d_data)
+print(colored('*'*25, 'red'))
 print("Via Theano:", f(a_data, b_data, c_data, d_data))
