d3c78c034f111983b29b924653281d786dcd8873
greatabel <myreceiver2for2github@gmail.com>
2017-06-08 15:45:49 +0800

    i8visualise neural network.py

--- Deep Learning with Python- A Hands-on Introduction/ch3Feed Forward Neural Networks/i7my try 2-Layer.py
+++ Deep Learning with Python- A Hands-on Introduction/ch3Feed Forward Neural Networks/i7my try 2-Layer.py
@@ -4,9 +4,9 @@ import autograd.numpy.random as npr
 from termcolor import colored
 
 examples = 4
-features = 2
+features = 3
 D = (npr.randn(examples, features), npr.randn(examples))
-print(colored('输入数据和输出数据 =>', 'red'), D)
+print(colored('输入数据和输出数据 =>', 'red'), D[0],'\n\nD[1]->', D[1])
 
 layer1_units = 2
 layers_units = 1
@@ -16,4 +16,19 @@ w2 = npr.rand(layer1_units, layers_units)
 b2 = 0.0
 
 theta = (w1, b1, w2, b2)
-print(colored('theta =>', 'blue'), theta)
+print(colored('theta =>', 'blue'), theta[0],'\ntheta[1]->',theta[1],
+    '\ntheta[2]->',theta[2],'\ntheta[3]->',theta[3])
+
+def squared_loss(y, y_hat):
+    return np.dot((y - y_hat), (y - y_hat))
+
+def neural_network(x, theta):
+    w1, b1, w2, b2 = theta
+    return np.tanh(np.dot((np.tanh(np.dot(x,w1) + b1)), w2) + b2)
+
+# def objective(theta, idx):
+
+if __name__ == "__main__":
+    print('D[0][0]=', D[0][0])
+    print('D[0][1]=', D[0][1])
+
