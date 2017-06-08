6c7541d8a973bdd75c5646abe54e4d4c5ba01960
greatabel <myreceiver2for2github@gmail.com>
2017-06-08 17:17:03 +0800

    i4activation functions.py tanh

--- Deep Learning with Python- A Hands-on Introduction/ch4Introduction to Theano/i4activation functions.py
+++ Deep Learning with Python- A Hands-on Introduction/ch4Introduction to Theano/i4activation functions.py
@@ -6,4 +6,10 @@ a = T.dmatrix('a')
 f_a = T.nnet.sigmoid(a)
 f_sigmoid = function([a],[f_a])
 
-print("sigmod:", f_sigmoid([[-1, 0, -1]]) )
+print("sigmod:", f_sigmoid([[-1, 0, 1]]) )
+
+# tanh
+b = T.dmatrix('b')
+f_b = T.tanh('b')
+f_tanh = function([b],[f_b])
+print("tanh:", f_tanh([[-1,0,1]]))
