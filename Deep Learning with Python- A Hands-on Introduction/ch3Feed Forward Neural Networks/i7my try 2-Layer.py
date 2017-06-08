0ed59eeca62a80f16882e5e2ae726680c03b2025
greatabel <myreceiver2for2github@gmail.com>
2017-06-08 15:14:25 +0800

    this is what I understand

--- Deep Learning with Python- A Hands-on Introduction/ch3Feed Forward Neural Networks/i7my try 2-Layer.py
+++ Deep Learning with Python- A Hands-on Introduction/ch3Feed Forward Neural Networks/i7my try 2-Layer.py
@@ -8,3 +8,12 @@ features = 2
 D = (npr.randn(examples, features), npr.randn(examples))
 print(colored('输入数据和输出数据 =>', 'red'), D)
 
+layer1_units = 2
+layers_units = 1
+w1 = npr.rand(features, layer1_units)
+b1 = npr.rand(layer1_units)
+w2 = npr.rand(layer1_units, layers_units)
+b2 = 0.0
+
+theta = (w1, b1, w2, b2)
+print(colored('theta =>', 'blue'), theta)
