d33ca34e4c082284b5df0c61c2d23e9ad5323159
greatabel <myreceiver2for2github@gmail.com>
2017-06-10 18:14:59 +0800

    i11Lost Function.py

--- Deep Learning with Python- A Hands-on Introduction/ch4Introduction to Theano/i11Lost Function.py
+++ Deep Learning with Python- A Hands-on Introduction/ch4Introduction to Theano/i11Lost Function.py
@@ -5,7 +5,9 @@ a1 = T.dmatrix('a1')
 a2 = T.dmatrix('a2')
 
 f_a = T.nnet.binary_crossentropy(a1, a2).mean()
-f_sigmoid = function([a1, a2], [f_a])
+f_sigmoid = function(inputs = [a1, a2], outputs = [f_a])
 
 print("Binary Cross Entropy [[0.01,0.01,0.01]],[[0.99,0.99,0.01]]:",
-        f_sigmoid([[0.01,0.01,0.01]],[[0.99,0.99,0.01]]) )
+        f_sigmoid(
+            [[0.01,0.01,0.01]],[[0.99,0.99,0.01]]) 
+        )
