2328e1a255892be6afd449090ae1cdb366caf43e
greatabel <myreceiver2for2github@gmail.com>
2017-06-10 18:55:43 +0800

    categorical_crossentropy

--- Deep Learning with Python- A Hands-on Introduction/ch4Introduction to Theano/i11Lost Function.py
+++ Deep Learning with Python- A Hands-on Introduction/ch4Introduction to Theano/i11Lost Function.py
@@ -9,5 +9,21 @@ f_sigmoid = function(inputs = [a1, a2], outputs = [f_a])
 
 print("Binary Cross Entropy [[0.01,0.01,0.01]],[[0.99,0.99,0.01]]:",
         f_sigmoid(
-            [[0.01,0.01,0.01]],[[0.99,0.99,0.01]]) 
+            [[0.01,0.01,0.01]],
+            [[0.99,0.99,0.01]]
+                 ) 
+        )
+
+# categroical cross entropy
+b1 = T.dmatrix('b1')
+b2 = T.dmatrix('b2')
+
+f_b = T.nnet.categorical_crossentropy(b1, b2)
+f_sigmoid = function([b1, b2], [f_b])
+
+print("Categroical Cross Entropy [[0.01,0.01,0.01]],[[0.99,0.99,0.01]]:",
+        f_sigmoid(
+            [[0.01,0.01,0.01]],
+            [[0.99,0.99,0.01]]
+                 ) 
         )
