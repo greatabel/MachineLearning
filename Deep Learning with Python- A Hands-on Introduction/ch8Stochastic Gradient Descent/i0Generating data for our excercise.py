62e122738d39c08d505f2b8fe64b80ee9ff69018
greatabel <myreceiver2for2github@gmail.com>
2017-06-20 16:16:36 +0800

    downhill

--- Deep Learning with Python- A Hands-on Introduction/ch8Stochastic Gradient Descent/i0Generating data for our excercise.py
+++ Deep Learning with Python- A Hands-on Introduction/ch8Stochastic Gradient Descent/i0Generating data for our excercise.py
@@ -2,7 +2,8 @@ import sklearn.datasets
 import numpy
 import pylab
 import theano.tensor as T
-import theano 
+import theano
+import downhill
 
 #Specifiy the number of examples we need (5000) and the noise level
 train_X, train_y = sklearn.datasets.make_moons(5000, noise=0.1)
@@ -12,6 +13,8 @@ train_y_onehot = numpy.eye(2)[train_y]
 pylab.scatter(train_X[:-1000, 0], train_X[:-1000, 1], c=train_y[:-1000], cmap=pylab.
 cm.Spectral)
 # pylab.show()
+
+
 
 #Set Seed
 numpy.random.seed(0)
@@ -43,3 +46,25 @@ loss_reg = 1./batch_size * reg_lambda/2 * (T.sum(T.sqr(W1)) + T.sum(T.sqr(W2)))
 loss = T.nnet.categorical_crossentropy(y_hat, y).mean() + loss_reg
 prediction = T.argmax(y_hat, axis=1)
 predict = theano.function([X], prediction)
+
+
+#Store the training and vlidation loss
+train_loss = []
+validation_loss = []
+opt = downhill.build('sgd', loss=loss)
+#Set up training and validation dataset splits, use only one example in a batch 
+#and use only one batch per step/epoc
+#Use everything except last 1000 examples for training
+train = downhill.Dataset([train_X[:-1000], train_y_onehot[:-1000]], batch_size=batch_size,
+iteration_size=1)
+#Use last 1000 examples for valudation
+valid = downhill.Dataset([train_X[-1000:], train_y_onehot[-1000:]])
+#SGD
+iterations = 0
+for tm, vm in opt.iterate(train, valid, patience=10000):
+    iterations += 1
+    # Record the training and validation loss
+    train_loss.append(tm['loss'])
+    validation_loss.append(vm['loss'])
+    if iterations > 10000:
+        break
