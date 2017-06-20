be9fe6c603262ce0df4b0ad5d133417ee25a1f7c
greatabel <myreceiver2for2github@gmail.com>
2017-06-20 16:07:09 +0800

    seed

--- Deep Learning with Python- A Hands-on Introduction/ch8Stochastic Gradient Descent/i0Generating data for our excercise.py
+++ Deep Learning with Python- A Hands-on Introduction/ch8Stochastic Gradient Descent/i0Generating data for our excercise.py
@@ -1,7 +1,8 @@
 import sklearn.datasets
 import numpy
 import pylab
-
+import theano.tensor as T
+import theano 
 
 #Specifiy the number of examples we need (5000) and the noise level
 train_X, train_y = sklearn.datasets.make_moons(5000, noise=0.1)
@@ -10,4 +11,35 @@ train_y_onehot = numpy.eye(2)[train_y]
 #Plot the data
 pylab.scatter(train_X[:-1000, 0], train_X[:-1000, 1], c=train_y[:-1000], cmap=pylab.
 cm.Spectral)
-pylab.show()
+# pylab.show()
+
+#Set Seed
+numpy.random.seed(0)
+num_examples = len(train_X)
+#Our Neural Network
+nn_input_dim = 2
+nn_hdim = 1000
+nn_output_dim = 2
+#Regularization
+reg_lambda = numpy.float64(0.01)
+#Weights and bias terms
+W1_val = numpy.random.randn(nn_input_dim, nn_hdim)
+b1_val = numpy.zeros(nn_hdim)
+W2_val = numpy.random.randn(nn_hdim, nn_output_dim)
+b2_val = numpy.zeros(nn_output_dim)
+X = T.matrix('X')
+y = T.matrix('y')
+W1 = theano.shared(W1_val, name='W1')
+b1 = theano.shared(b1_val, name='b1')
+W2 = theano.shared(W2_val, name='W2')
+b2 = theano.shared(b2_val, name='b2')
+batch_size = 1
+#Our Loss function
+z1 = X.dot(W1) + b1
+a1 = T.tanh(z1)
+z2 = a1.dot(W2) + b2
+y_hat = T.nnet.softmax(z2)
+loss_reg = 1./batch_size * reg_lambda/2 * (T.sum(T.sqr(W1)) + T.sum(T.sqr(W2)))
+loss = T.nnet.categorical_crossentropy(y_hat, y).mean() + loss_reg
+prediction = T.argmax(y_hat, axis=1)
+predict = theano.function([X], prediction)
