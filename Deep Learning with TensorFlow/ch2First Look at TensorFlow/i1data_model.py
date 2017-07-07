9d6271173c9b31eeab847b647e2afffeb07740b8
greatabel <myreceiver2for2github@gmail.com>
2017-07-07 16:33:00 +0800

    i1data_model.py

---
+++ Deep Learning with TensorFlow/ch2First Look at TensorFlow/i1data_model.py
@@ -0,0 +1,14 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+import tensorflow as tf
+
+scalar = tf.constant(100)
+vector = tf.constant([1, 2, 3, 4, 5])
+matrix = tf.constant([[1, 2, 3], [4, 5, 6]])
+cube_matrix = tf.constant([[[1], [2], [3]], [[4], [5], [6]], [[7], [8], [9]]])
+
+print(scalar.get_shape())
+print(vector.get_shape())
+print(matrix.get_shape())
+print(cube_matrix.get_shape())
