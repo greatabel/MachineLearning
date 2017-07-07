868ac3e38c8d50aacde00a2b86d6ae0a4d7d3155
greatabel <myreceiver2for2github@gmail.com>
2017-07-07 16:41:46 +0800

    tf.Variable

--- Deep Learning with TensorFlow/ch2First Look at TensorFlow/i2tensor_with_numpy.py
+++ Deep Learning with TensorFlow/ch2First Look at TensorFlow/i2tensor_with_numpy.py
@@ -8,3 +8,12 @@ tensor_1d = tf.constant(tensor_1d)
 with tf.Session() as sess:
     print(tensor_1d.get_shape())
     print(sess.run(tensor_1d))
+
+print('*-' * 20)
+# tensor 2d variable
+tensor_2d = np.array([(1, 2, 3), (4, 5, 6), (7, 8, 9)])
+tensor_2d = tf.Variable(tensor_2d)
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    print(tensor_2d.get_shape())
+    print(sess.run(tensor_2d))
