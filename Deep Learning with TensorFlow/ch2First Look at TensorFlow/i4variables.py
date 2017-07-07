7ad6c2c8f362bb3f5077580abdc117c802930aa1
greatabel <myreceiver2for2github@gmail.com>
2017-07-07 16:58:31 +0800

    i4variables.py

---
+++ Deep Learning with TensorFlow/ch2First Look at TensorFlow/i4variables.py
@@ -0,0 +1,18 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import tensorflow as tf
+
+value = tf.Variable(0, name='Value')
+
+one = tf.constant(1)
+new_value = tf.add(value, one)
+update_value = tf.assign(value, new_value)
+
+initialize_var = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(initialize_var)
+    print('init', sess.run(value))
+    for _ in range(10):
+        sess.run(update_value)
+        print(sess.run(value))
