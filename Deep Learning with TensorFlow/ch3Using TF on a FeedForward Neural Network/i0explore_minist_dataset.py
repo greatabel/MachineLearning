3509055455d593875a9cd96dc2a1d658a245b3c2
greatabel <myreceiver2for2github@gmail.com>
2017-07-15 23:38:52 +0800

    MNIST_data

--- Deep Learning with TensorFlow/ch3Using TF on a FeedForward Neural Network/i0explore_minist_dataset.py
+++ Deep Learning with TensorFlow/ch3Using TF on a FeedForward Neural Network/i0explore_minist_dataset.py
@@ -10,9 +10,10 @@ import numpy
 # https://github.com/tensorflow/tensorflow/blob/master/tensorflow/examples/tutorials/mnist/input_data.py
 from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
 
-mypath = "/Users/wanchang/Downloads/AbelProject/" + \
-"MachineLearning/Deep Learning with TensorFlow/" + \
-"ch3Using TF on a FeedForward Neural Network/MNIST_data/"
+# mypath = "/Users/wanchang/Downloads/AbelProject/" + \
+# "MachineLearning/Deep Learning with TensorFlow/" + \
+# "ch3Using TF on a FeedForward Neural Network/MNIST_data/"
+mypath = "./MNIST_data/"
 __input = read_data_sets(mypath)
 print("__input.train.images.shape=", __input.train.images.shape)
 print("__input.train.labels.shape=", __input.train.labels.shape)
