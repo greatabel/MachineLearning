680756aa3fc80b3476deed544276a1e38a8607b8
greatabel <myreceiver2for2github@gmail.com>
2017-07-15 23:37:39 +0800

    make mnist dataset working

---
+++ Deep Learning with TensorFlow/ch3Using TF on a FeedForward Neural Network/i0explore_minist_dataset.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# import mnist_data
+
+import gzip
+import os
+import tempfile
+
+import numpy
+# https://github.com/tensorflow/tensorflow/blob/master/tensorflow/examples/tutorials/mnist/input_data.py
+from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
+
+mypath = "/Users/wanchang/Downloads/AbelProject/" + \
+"MachineLearning/Deep Learning with TensorFlow/" + \
+"ch3Using TF on a FeedForward Neural Network/MNIST_data/"
+__input = read_data_sets(mypath)
+print("__input.train.images.shape=", __input.train.images.shape)
+print("__input.train.labels.shape=", __input.train.labels.shape)
+
