bd699087555487c2ad998d072b64e643f9791981
greatabel <myreceiver2for2github@gmail.com>
2017-07-16 10:11:21 +0800

    __input.test.images.shape

--- Deep Learning with TensorFlow/ch3Using TF on a FeedForward Neural Network/i0explore_minist_dataset.py
+++ Deep Learning with TensorFlow/ch3Using TF on a FeedForward Neural Network/i0explore_minist_dataset.py
@@ -17,4 +17,6 @@ mypath = "./MNIST_data/"
 __input = read_data_sets(mypath)
 print("__input.train.images.shape=", __input.train.images.shape)
 print("__input.train.labels.shape=", __input.train.labels.shape)
+print("__input.test.images.shape=", __input.test.images.shape)
+print("__input.test.labels.shape=", __input.test.labels.shape)
 
