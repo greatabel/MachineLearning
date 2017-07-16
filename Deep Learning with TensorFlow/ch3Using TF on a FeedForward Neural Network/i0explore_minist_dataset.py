a4abb5ad07a38250fc3ca140f0e9cbff8c9defb0
greatabel <myreceiver2for2github@gmail.com>
2017-07-16 10:38:02 +0800

    plt.show()

--- Deep Learning with TensorFlow/ch3Using TF on a FeedForward Neural Network/i0explore_minist_dataset.py
+++ Deep Learning with TensorFlow/ch3Using TF on a FeedForward Neural Network/i0explore_minist_dataset.py
@@ -20,3 +20,11 @@ print("__input.train.labels.shape=", __input.train.labels.shape)
 print("__input.test.images.shape=", __input.test.images.shape)
 print("__input.test.labels.shape=", __input.test.labels.shape)
 
+image_0 = __input.train.images[0]
+image_0 = np.resize(image_0, (28,28))
+
+label_0 = __input.train.labels[0]
+print('label_0 ->', label_0)
+
+plt.imshow(image_0, cmap='Greys_r')
+plt.show()
