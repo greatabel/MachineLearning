747a374e596e50a48bed2951b91c90e69fa5c3fe
greatabel <myreceiver2for2github@gmail.com>
2017-07-19 16:34:43 +0800

    layer

--- Deep Learning with TensorFlow/ch3Using TF on a FeedForward Neural Network/i3five layers sigmoid.py
+++ Deep Learning with TensorFlow/ch3Using TF on a FeedForward Neural Network/i3five layers sigmoid.py
@@ -19,3 +19,13 @@ M = 100
 N = 60
 O = 30
 
+W1 = tf.Variable(tf.truncated_normal([784, L], stddev=0.1))  
+B1 = tf.Variable(tf.zeros([L]))
+W2 = tf.Variable(tf.truncated_normal([L, M], stddev=0.1))
+B2 = tf.Variable(tf.zeros([M]))
+W3 = tf.Variable(tf.truncated_normal([M, N], stddev=0.1))
+B3 = tf.Variable(tf.zeros([N]))
+W4 = tf.Variable(tf.truncated_normal([N, O], stddev=0.1))
+B4 = tf.Variable(tf.zeros([O]))
+W5 = tf.Variable(tf.truncated_normal([O, 10], stddev=0.1))
+B5 = tf.Variable(tf.zeros([10]))
