eb3771700a645b009528623ba977bf27966324ca
greatabel <myreceiver2for2github@gmail.com>
2017-08-09 15:28:56 +0800

    placeholder

--- Deep Learning with TensorFlow/ch4TF on a Convolutional Neural Network/i1first_CNN.py
+++ Deep Learning with TensorFlow/ch4TF on a Convolutional Neural Network/i1first_CNN.py
@@ -4,4 +4,15 @@ os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
 
+
+batch_size = 128
+test_size = 256
+
+img_size = 28
+num_classes = 10
+
+
+X = tf.placeholder("float", [None, img_size, img_size, 1])
+Y = tf.placeholder("float", [None, num_classes])
