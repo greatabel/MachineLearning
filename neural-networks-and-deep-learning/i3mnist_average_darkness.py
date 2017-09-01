9dce35d5d53ef8a9161f60c2dfafd003c84d303e
greatabel <myreceiver2for2github@gmail.com>
2017-09-01 22:54:46 +0800

    avg_darkness

--- neural-networks-and-deep-learning/i3mnist_average_darkness.py
+++ neural-networks-and-deep-learning/i3mnist_average_darkness.py
@@ -4,11 +4,20 @@ import i2mnist_loader
 
 def main():
     training_data, validation_data, test_data = i2mnist_loader.load_data()
-    print(training_data.shape, validation_data.shape, test_data.shape)
+    avgs = avg_darkness(training_data)
+    print(avgs)
     return ""
 
 def avg_darkness(training_data):
-    return ""
+    digit_counts = defaultdict(int)
+    darknesses = defaultdict(float)
+    for image, digit in zip(training_data[0], training_data[1]):
+        digit_counts[digit] += 1
+        darknesses[digit] += sum(image)
+    avgs = defaultdict(float)
+    for digit, n in digit_counts.items():
+        avgs[digit] = darknesses[digit] / n
+    return avgs
 
 def guess_digit(image, avgs):
     return ""
