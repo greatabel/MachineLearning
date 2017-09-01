0db67b42f7b8a6a19028b49501007b7033aec764
greatabel <myreceiver2for2github@gmail.com>
2017-09-01 22:48:48 +0800

    i3mnist_average_darkness.py

--- neural-networks-and-deep-learning/i3mnist_average_darkness.py
+++ neural-networks-and-deep-learning/i3mnist_average_darkness.py
@@ -3,6 +3,8 @@ from collections import defaultdict
 import i2mnist_loader
 
 def main():
+    training_data, validation_data, test_data = i2mnist_loader.load_data()
+    print(training_data.shape, validation_data.shape, test_data.shape)
     return ""
 
 def avg_darkness(training_data):
