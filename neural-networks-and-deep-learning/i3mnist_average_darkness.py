3131acd37bcad349a1785878d27fdb00e8aab7d3
greatabel <myreceiver2for2github@gmail.com>
2017-09-01 22:45:01 +0800

    i3mnist_average_darkness.py

---
+++ neural-networks-and-deep-learning/i3mnist_average_darkness.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+import i2mnist_loader
+
+def main():
+    return ""
+
+def avg_darkness(training_data):
+    return ""
+
+def guess_digit(image, avgs):
+    return ""
+
+if __name__ == "__main__":
+    main()
