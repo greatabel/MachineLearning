1a0164f26b8de346d9686c23293c1d341305319a
greatabel <myreceiver2for2github@gmail.com>
2017-09-02 14:03:12 +0800

    SGD

--- neural-networks-and-deep-learning/i1network_test.py
+++ neural-networks-and-deep-learning/i1network_test.py
@@ -3,3 +3,25 @@ from  i1network  import Network, sigmoid
 net = Network([2,3,1])
 net.abel_print()
 print('sigmoid(10)=', sigmoid(10))
+
+import i2mnist_loader
+training_data, validation_data, test_data = i2mnist_loader.load_data_wrapper()
+training_data = list(training_data)
+
+'''
+# ---------------------
+# - network.py example:
+import network
+
+'''
+
+import i1network
+import time
+
+tic = time.clock()
+net = i1network.Network([784, 30, 10])
+
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+
+toc = time.clock()
+print("time=",toc - tic)
