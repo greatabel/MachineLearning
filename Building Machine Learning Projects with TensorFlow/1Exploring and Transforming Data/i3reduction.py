011f690874872711881ed7c2674b38f270d3ff36
greatabel <myreceiver2for2github@gmail.com>
2017-09-18 21:17:43 +0800

    i3reduction.py

---
+++ Building Machine Learning Projects with TensorFlow/1Exploring and Transforming Data/i3reduction.py
@@ -0,0 +1,20 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+import tensorflow as tf
+
+sess = tf.InteractiveSession() 
+x = tf.constant([[1,  2, 3], 
+                  [3,  2, 1], 
+                  [-1,-2,-3]]) 
+
+boolean_tensor = tf.constant(
+        [[True,  False, True], 
+          [False, False, True], 
+        [True, False, False]])
+print(tf.reduce_prod(x, reduction_indices=1).eval() )# reduce pro
+print(tf.reduce_min(x, reduction_indices=1).eval() )# reduce mi
+print(tf.reduce_max(x, reduction_indices=1).eval() )# reduce ma
+print(tf.reduce_mean(x, reduction_indices=1).eval() )# reduce mea
+print(tf.reduce_all(boolean_tensor, reduction_indices=1).eval() )# reduce al
+print(tf.reduce_any(boolean_tensor, reduction_indices=1).eval() )# reduce any 
