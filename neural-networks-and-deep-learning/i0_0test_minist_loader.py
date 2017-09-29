763f8966e37c6bd0480f9415c7a5cddf0dffcc9a
greatabel <myreceiver2for2github@gmail.com>
2017-09-29 17:26:55 +0800

    test zip func in python3

--- neural-networks-and-deep-learning/i0_0test_minist_loader.py
+++ neural-networks-and-deep-learning/i0_0test_minist_loader.py
@@ -3,6 +3,12 @@ import i2mnist_loader
 def main():
     training_data, validation_data, test_data = i2mnist_loader.load_data()
     
+def test_zip():
+    a = [1,2,3,4]
+    b = [5,6,7,8]
+    c = zip(a,b)
+    print(a,b,type(c),list(c))
 
 if __name__ == "__main__":
+    test_zip()
     main()
