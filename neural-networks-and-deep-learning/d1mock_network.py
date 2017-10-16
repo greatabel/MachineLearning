4c70b8e91dc17500592f76a01191c435e8b59448
greatabel <myreceiver2for2github@gmail.com>
2017-10-16 22:46:04 +0800

    SGD

--- neural-networks-and-deep-learning/d1mock_network.py
+++ neural-networks-and-deep-learning/d1mock_network.py
@@ -2,6 +2,12 @@ import random
 import numpy as np
 from termcolor import colored
 
+def demo_shuffle():
+    arr = np.arange(10)
+    print('arr->', arr)
+    np.random.shuffle(arr)
+    print('arr->', arr)
+
 def mock_data():
     result = []
     for i in range(10):
@@ -28,14 +34,23 @@ class Network(object):
 
     def SGD(self, training_data, epochs, mini_batch_size, eta,
                 test_data=None):
-        return ""
-# 0 0 1
-# 0 0 0    -> a,b 之一
-# 0 0 1
+        training_data = list(training_data)
+        n = len(training_data)
+        print(colored('n =>','red'),n)
+        if test_data:
+            test_data = list(test_data)
+            n_test = len(test_data)
+        # for j in range(epochs):
+        #     print('-')
+
 
 if __name__ == "__main__":
+    # demo_shuffle()
+    # 0 0 1
+    # 0 0 0    -> a,b 之一
+    # 0 0 1
     training_data, validation_data, test_data = mock_data(),mock_data(),mock_data()
     net = Network([3*3, 4, 2])
     net.abel_print()
-    net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+    net.SGD(training_data, 30, 5, 3.0, test_data=test_data)
     # net.SGD(training_data, 3, 2, 3.0, test_data=test_data)
