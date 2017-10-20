e7c6216d6f5eaa51087815a5432ebbad5df623d6
greatabel <myreceiver2for2github@gmail.com>
2017-10-20 16:40:28 +0800

    decision making

--- neural-networks-and-deep-learning/d2perceptron.py
+++ neural-networks-and-deep-learning/d2perceptron.py
@@ -1,16 +1,19 @@
 import random
 import numpy as np
+from termcolor import colored
 
+questions = ['whether is fine?', 'Boy friend or girl friend with you?',
+             'Place is near your location?']
 def single_perceptron(x):
     # print('-' * 20 )
     # x = [10, 20, 30, 40, 50]
-    print('x=', x)
-    w = np.random.rand(1,5)
-    threshold = 100
+
+    w = np.random.rand(1,3)
+    threshold = 1
     output = None
     # print('w ->', w, w.shape)
     w = w.T
-    print('w ->', w, w.shape)
+    print('w (importance or weight)->', w, w.shape)
     isum = np.dot(x, w)
     print('sum=', isum)
 
@@ -23,9 +26,11 @@ def single_perceptron(x):
 
 
 def main():
-    for i in range(10):
-        print('#'*10, i)
-        x = np.random.randint(3, size=5) * 100
+    for i in range(5):
+        print(colored('i =>','red'), i)
+        x = np.random.randint(2, size=3)
+        for j in zip(questions, x):
+            print(j[0],"->", "Yes" if j[1] == 1 else "No")
         single_perceptron(x)
 
 
