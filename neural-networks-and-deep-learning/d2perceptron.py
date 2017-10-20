0a5a5461ffbd11b23fdc5a8b36637de939303b75
greatabel <myreceiver2for2github@gmail.com>
2017-10-20 12:19:49 +0800

    single_perceptron

--- neural-networks-and-deep-learning/d2perceptron.py
+++ neural-networks-and-deep-learning/d2perceptron.py
@@ -2,10 +2,32 @@ import random
 import numpy as np
 
 def single_perceptron(x):
+    # print('-' * 20 )
+    # x = [10, 20, 30, 40, 50]
+    print('x=', x)
+    w = np.random.rand(1,5)
+    threshold = 100
+    output = None
+    # print('w ->', w, w.shape)
+    w = w.T
+    print('w ->', w, w.shape)
+    isum = np.dot(x, w)
+    print('sum=', isum)
+
+    if isum <= threshold:
+        output = 0
+    else:
+        output = 1
+    print(output)
+
+
 
 def main():
-    
-    return ""
+    for i in range(10):
+        print('#'*10, i)
+        x = np.random.randint(2, size=5) * 10
+        single_perceptron(x)
+
 
 if __name__ == "__main__":
     main()
