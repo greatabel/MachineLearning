fa2d6e9bbb11fdaeb8b98336c6f2f397392c1b7a
greatabel <myreceiver2for2github@gmail.com>
2017-10-23 17:00:30 +0800

    finish sum & carry

--- neural-networks-and-deep-learning/d3simulate NAND gate.py
+++ neural-networks-and-deep-learning/d3simulate NAND gate.py
@@ -21,13 +21,24 @@ def main():
     # print(t.threshold,'#', t.output)
     threshold = 3
     weight = -2
-    nodelist = [Node(i, threshold) for i in range(4)]
+    nodelist = [Node(i, threshold) for i in range(5)]
     for x1 in (0, 1):
         for x2 in (0, 1):
-
+            # if x1 == 1 and x2 == 1:
             nodelist[0].input([x1 * weight, x2 * weight])
             n0 = nodelist[0].output
-            print('x1, x2 =>',x1, x2, 'n0=>', n0)
+            # print('x1, x2 =>',x1, x2, 'n0=>', n0)
+            nodelist[1].input([x1 * weight, n0 * weight])
+            n1 = nodelist[1].output
+            # print('n1 * weight, x2 * weight=>', n1 * weight, x2 * weight)
+            nodelist[2].input([n0 * weight, x2 * weight])
+            n2 = nodelist[2].output
+            nodelist[3].input([n0 * weight, n0 * weight])
+            n3 = nodelist[3].output
+            # print('n1, n2', n1, n2)
+            nodelist[4].input([n1 * weight, n2 * weight])
+            n4 = nodelist[4].output
+            print('x1, x2 =>', x1, x2,'#', n4, '#', n3)
     
 
 if __name__ == "__main__":
