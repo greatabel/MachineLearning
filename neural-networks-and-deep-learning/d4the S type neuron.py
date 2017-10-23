3bb41c8674e2083bda1ba89f3ec647dc771f6315
greatabel <myreceiver2for2github@gmail.com>
2017-10-23 17:41:44 +0800

    d4the S type neuron.py

---
+++ neural-networks-and-deep-learning/d4the S type neuron.py
@@ -0,0 +1,27 @@
+import numpy as np
+from termcolor import colored
+
+class Node(object):
+
+    def __init__(self, id, threshold):
+        self.id = id
+        self.threshold = threshold
+
+    def input(self, w, x):
+        w = np.array(w)
+        x = np.array(x)
+        w = w.T
+        isum = np.dot(x, w)
+        result = 1 if isum + self.threshold > 0 else 0
+        self.output = result
+
+def main():
+    t = Node('id1', 10)
+    x = [-2, -2, -2]
+    w = [3, 3, 3]
+    t.input(w, x)
+    print(t.threshold,'#', t.output)
+
+
+if __name__ == "__main__":
+    main()
