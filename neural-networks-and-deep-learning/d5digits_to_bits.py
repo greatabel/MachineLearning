0831713afc6491c5b744f2ded616bd0fe34c9175
greatabel <myreceiver2for2github@gmail.com>
2017-10-25 10:28:49 +0800

    d5digits_to_bits.py inputs

--- neural-networks-and-deep-learning/d5digits_to_bits.py
+++ neural-networks-and-deep-learning/d5digits_to_bits.py
@@ -15,7 +15,7 @@ class Node(object):
         isum = np.dot(x, w)
         # result = 1 if isum + self.threshold > 0 else 0
         z = isum + self.threshold
-        result = sigmoid(z)
+        result = step_function(z)
         self.output = result
 
 def step_function(z):
@@ -23,7 +23,13 @@ def step_function(z):
     return result
     
 def main():
-    ""
+    # 设置阀值接近于0, 权重随便设置成很大的值
+    threshold = 0.01
+    weight = -10
+    nodelist = [Node(i, threshold) for i in range(10)]
+    for i in range(10):
+        inputs = [1 if i==j else 0  for j in range(10)]
+        print(inputs)
 
 if __name__ == "__main__":
     main()
