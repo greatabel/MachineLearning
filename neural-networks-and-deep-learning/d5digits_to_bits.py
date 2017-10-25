3d2a3760a3f9d0c90a2228afa4e80494f1bef341
greatabel <myreceiver2for2github@gmail.com>
2017-10-25 14:38:05 +0800

    final version of d5digits_to_bits.py

--- neural-networks-and-deep-learning/d5digits_to_bits.py
+++ neural-networks-and-deep-learning/d5digits_to_bits.py
@@ -2,11 +2,14 @@ import numpy as np
 from termcolor import colored
 
 my_hardcode_weights = {
-    0: [0, 0, 0, 0],
-    1: [0, 0, 0, 1],
-    2: [0, 0, 1, 0],
-    3: [0, 0, 1, 1],
-    4: [0, 1, 0, 0]
+    # 0: [0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    # 1: [0, 0, 0, 0, 1, 1, 1, 1, 0, 0],
+    # 2: [0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+    # 3: [0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+    0: [1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+    1: [1, 1, 1, 1, 0, 0, 0, 0, 1, 1],
+    2: [1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
+    3: [1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
 }
 
 class Node(object):
@@ -14,7 +17,9 @@ class Node(object):
     def __init__(self, id, threshold):
         self.id = id
         self.threshold = threshold
-        self.weights = my_hardcode_weights[id]
+
+    def set_weights(self, w):
+        self.weights = w
 
     def input(self, w, x):
         # print('input w, x=>', w, x)
@@ -30,21 +35,29 @@ def step_function(z):
     return result
     
 def main():
+    print(np.array([10,20]), np.array([10,20]) * 10)
     # 设置阀值接近于0, 权重随便设置成很大的值
     threshold = 0.01
     weight = -10
-    layer1_length = 5
+    layer1_length = 10
     layer2_length = 4
     nodelist_layer1 = [Node(i, threshold) for i in range(layer1_length)]
     nodelist_layer2 = [Node(i, threshold) for i in range(layer2_length)]
+    for key, value in my_hardcode_weights.items():
+        nodelist_layer2[key].set_weights(value)
+
     for i in range(layer1_length):
-        inputs = [0 if i==j else 1  for j in range(layer1_length)]
-        print('i=', i, 'inputs=>', inputs)
+        inputs = [1 if i==j else 0  for j in range(layer1_length)]
+        # inputs = [0 if i==j else 1  for j in range(layer1_length)]
+        # print('i=', i, 'inputs=>', inputs)
         # 对于输入层的上一层 输入值0/1， 只有单连接权重是-10
-        for node in nodelist_layer1:
-            node.input(np.array([-10]), np.array([inputs[node.id]]))
+        outputs = []
+        for node in nodelist_layer2:
+            node.input(np.array([node.weights]) * weight, np.array([inputs]))
             output = node.output
-            print('no#', node.id,'output>', output, node.weights)
+            outputs.append(output)
+            # print('no#', node.id,'output>', output, node.weights)
+        print('1s layer output, 2d layer input >', outputs)
         
 
         
