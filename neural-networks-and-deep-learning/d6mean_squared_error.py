4c74a9cfb4c6f44ae6d58067b7b5efc2b9fadfa3
greatabel <myreceiver2for2github@gmail.com>
2017-10-25 14:59:24 +0800

    d6mean_squared_error.py

--- neural-networks-and-deep-learning/d6mean_squared_error.py
+++ neural-networks-and-deep-learning/d6mean_squared_error.py
@@ -1,8 +1,17 @@
 import numpy as np
 from termcolor import colored
 
+def mse(y, y_pred):
+    mse = np.mean((y - y_pred)**2)
 
+def generate_test_data():
+    a = 2
+    b = 3
+    x = np.random.randn(10, 1)
+    print(x, type(x))
+    f = a * x + b
 def main():
+
     print(colored('result =>', 'red'))
 
 if __name__ == "__main__":
