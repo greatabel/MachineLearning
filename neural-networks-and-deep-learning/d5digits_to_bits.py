3002620233438cd8dd0055470dbc5895e93b9bd4
greatabel <myreceiver2for2github@gmail.com>
2017-10-25 11:38:03 +0800

    d5digits_to_bits.py 1st layer

--- neural-networks-and-deep-learning/d5digits_to_bits.py
+++ neural-networks-and-deep-learning/d5digits_to_bits.py
@@ -1,16 +1,23 @@
 import numpy as np
 from termcolor import colored
 
-
+my_hardcode_weights = {
+    0: [0, 0, 0, 0],
+    1: [0, 0, 0, 1],
+    2: [0, 0, 1, 0],
+    3: [0, 0, 1, 1],
+    4: [0, 1, 0, 0]
+}
 
 class Node(object):
 
     def __init__(self, id, threshold):
         self.id = id
         self.threshold = threshold
+        self.weights = my_hardcode_weights[id]
 
     def input(self, w, x):
-
+        # print('input w, x=>', w, x)
         w = w.T
         isum = np.dot(x, w)
         # result = 1 if isum + self.threshold > 0 else 0
@@ -26,10 +33,21 @@ def main():
     # 设置阀值接近于0, 权重随便设置成很大的值
     threshold = 0.01
     weight = -10
-    nodelist = [Node(i, threshold) for i in range(10)]
-    for i in range(10):
-        inputs = [1 if i==j else 0  for j in range(10)]
-        print(inputs)
+    layer1_length = 5
+    layer2_length = 4
+    nodelist_layer1 = [Node(i, threshold) for i in range(layer1_length)]
+    nodelist_layer2 = [Node(i, threshold) for i in range(layer2_length)]
+    for i in range(layer1_length):
+        inputs = [0 if i==j else 1  for j in range(layer1_length)]
+        print('i=', i, 'inputs=>', inputs)
+        # 对于输入层的上一层 输入值0/1， 只有单连接权重是-10
+        for node in nodelist_layer1:
+            node.input(np.array([-10]), np.array([inputs[node.id]]))
+            output = node.output
+            print('no#', node.id,'output>', output, node.weights)
+        
+
+        
 
 if __name__ == "__main__":
     main()
