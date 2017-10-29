35f75c526e937fb4f17989b9c6605951f2b5d048
greatabel <myreceiver2for2github@gmail.com>
2017-10-29 17:21:51 +0800

    different scope

--- neural-networks-and-deep-learning/d7mini batch vs all.py
+++ neural-networks-and-deep-learning/d7mini batch vs all.py
@@ -1,16 +1,36 @@
 from  d6mean_squared_error import mse, generate_test_data
 from termcolor import colored
 
+def batch(l, batch_size):
+    n = len(l)
+    # print('n =>', n, 'l =>', l)
+    mini_batches = [
+        l[k:k+batch_size]
+        for k in range(0, n, batch_size)
+    ]
+    # print('mini_batches =>', mini_batches)
+    return mini_batches
 
 def main():
     a = 2
-    b = 10
-    noise = a / 10.0    
-    x, y_pred = generate_test_data(a + noise, b + noise, 100)
-    y = a * x + b
-    
+    b = 3
+    noise = a / 10.0
+    for scope in [10, 100, 1000, 10000]:
+        print(colored('scope =>', 'blue'), scope )
+        data_size = scope
+        # print('noise=>', noise)
+        x, y_pred = generate_test_data(a + noise, b + noise, data_size)
+        y = a * x + b
+        all_mse = mse(y, y_pred)
+        print(colored(' mse(y, y_pred) =>', 'red'), all_mse )
+        batch_size = data_size // 5
 
-    print(colored(' mse(y, y_pred) =>', 'red'), mse(y, y_pred))
+        y_group = batch(y, batch_size)
+        y_pred_group = batch(y_pred, batch_size)
+        for i in range(len(y_group)):
+            batch_mse = mse(y_group[i], y_pred_group[i])
+            print(colored(' batch_mse[' + str(i) + '] =>', 'red'), 
+                    batch_mse, "{0:.0f}%".format(100 * abs(batch_mse - all_mse)/ all_mse))
 
 if __name__ == "__main__":
     main()
