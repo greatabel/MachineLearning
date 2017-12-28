f397cb26f7af8f63d36fd71e56d7529742366c2b
greatabel <myreceiver2for2github@gmail.com>
2017-12-28 17:46:24 +0800

    split and test

--- Path_of_ML/ch1ML/i1iris.py
+++ Path_of_ML/ch1ML/i1iris.py
@@ -21,3 +21,32 @@ print(iris.isnull().sum())
 
 # 观察样本中按类别数量是否比较均衡
 print(iris.groupby('y').count())
+
+# 选择全部特征训练模型
+X = iris[scikit_iris.feature_names]
+# label
+y = iris['y']
+
+# 第一步，选择model
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=1)
+# 第二步，fit X、y
+knn.fit(X, y)
+# 第三步，predict新数据
+print('knn.predict([[3, 2, 2, 5]])=', knn.predict([[3, 2, 2, 5]]) )
+
+# 分割训练集和测试集
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
+# 分割训练-测试集
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=4)
+
+# K=15
+knn = KNeighborsClassifier(n_neighbors=15)
+knn.fit(X_train, y_train)
+
+y_pred_on_train = knn.predict(X_train)
+y_pred_on_test = knn.predict(X_test)
+# print(metrics.accuracy_score(y_train, y_pred_on_train))
+print('accuracy: ：{}'.format(metrics.accuracy_score(y_test, y_pred_on_test)))
