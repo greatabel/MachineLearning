8bc119d2d7700934505c245dac09371db00ecefe
greatabel <myreceiver2for2github@gmail.com>
2018-01-28 12:35:07 +0800

    np.vstack

--- Path_of_ML/ch1ML/d0numpy_like_demo.py
+++ Path_of_ML/ch1ML/d0numpy_like_demo.py
@@ -4,11 +4,19 @@ x = np.arange(6)
 x = x.reshape((2,3))
 
 x_zero =  np.zeros_like(x)
-x_empty = np.empty_like(x)
 x_one   = np.ones_like(x)
-
+x_empty = np.empty_like(x)
 
 print(x)
 print('#'*5, x_zero)
 print('#'*10, x_one)
-print('#'*10,'return random is:', x_empty)
+print('#'*15,'return random is:', x_empty)
+
+print('-'*20)
+x = np.arange(-3.0, 6.0, 1)
+x_one = np.ones_like(x)
+print('@'*3, x)
+print('@'*6, x_one)
+print('@'*6, 0.2*x_one)
+x_vs = np.vstack([x, x_one, 0.2*x_one])
+print('@'*9, x_vs)
