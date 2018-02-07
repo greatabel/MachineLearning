ddfa0361dbab56a8914e895fb635e7948d2a7fcb
greatabel <myreceiver2for2github@gmail.com>
2018-02-07 21:28:08 +0800

    处理类别意义的特征

--- Path_of_ML/ch2ML_Advanced/i0load_titanic_csv.py
+++ Path_of_ML/ch2ML_Advanced/i0load_titanic_csv.py
@@ -6,3 +6,28 @@ print(data_train.info())
 
 sur_count = data_train.groupby('Survived').count()
 print(sur_count)
+
+h3 = data_train.head(3)
+print(h3)
+
+# 处理数据缺失
+def set_missing_ages(p_df):
+    p_df.loc[(p_df.Age.isnull()), 'Age'] = p_df.Age.dropna().mean()
+    return p_df
+
+df = set_missing_ages(data_train)
+
+
+# 归一化数值数据
+import sklearn.preprocessing as preprocessing
+
+scaler = preprocessing.StandardScaler()
+df['Age_scaled'] = scaler.fit_transform(data_train['Age'].values.reshape(-1, 1))
+df['Fare_scaled'] = scaler.fit_transform(data_train['Fare'].values.reshape(-1, 1))
+
+# 处理类别意义的特征
+def set_cabin_type(p_df):
+    p_df.loc[(p_df.Cabin.notnull()), 'Cabin'] = "Yes"
+    p_df.loc[(p_df.Cabin.isnull()), 'Cabin'] = "No"
+    return p_df
+df = set_cabin_type(df)
