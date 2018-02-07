586db17680cc44b9f66173f623e20e8e8a75b869
greatabel <myreceiver2for2github@gmail.com>
2018-02-07 09:20:13 +0800

    i0load_titanic_csv.py

--- Path_of_ML/ch2ML_Advanced/i0load_titanic_csv.py
+++ Path_of_ML/ch2ML_Advanced/i0load_titanic_csv.py
@@ -3,3 +3,6 @@ import pandas as pd # pandas是python的数据格式处理类库
 # 加载泰坦尼克号生存预测数据集
 data_train = pd.read_csv("../data/titanic/train.csv")
 print(data_train.info())
+
+sur_count = data_train.groupby('Survived').count()
+print(sur_count)
