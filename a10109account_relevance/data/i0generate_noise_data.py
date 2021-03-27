c18509dad79ef9a8618ea61435d0e8b771e91b69
greatabel <myreceiver2for2github@gmail.com>
2021-03-28 03:37:22 +0800

    noise data

---
+++ a10109account_relevance/data/i0generate_noise_data.py
@@ -0,0 +1,27 @@
+import random,string
+
+#生成大小字母和数字一起的大字符串
+all_str = string.ascii_letters+string.digits
+
+res_lit = []
+#生成100个
+for i in range(24):
+    #从大字符串中随机生成8位数的字符，换行并转换成字符串
+	res  =  ''.join(random.sample(all_str,8))
+	bio  = 'noise data '+ ''.join(random.sample(all_str,30))
+	if res not in res_lit:
+		if i > 3:
+			print(str(i)+","+res+",'上海','"+bio+"'")
+		res_lit.append((i,res))
+
+
+print('-'*30)
+
+for i in range(24):
+    #从大字符串中随机生成8位数的字符，换行并转换成字符串
+	res  =  ''.join(random.choice("0123456789") for i in range(16))
+	bio  = 'noise data '+ ''.join(random.sample(all_str,30))
+	if res not in res_lit:
+		if i > 3:
+			print(str(i)+","+res+",'上海','"+bio+"'")
+		res_lit.append((i,res))
--- a10109account_relevance/i2compare.py
+++ a10109account_relevance/i2compare.py
@@ -5,11 +5,12 @@ def load(filepath):
 	with open(filepath,'rt')as f:
 	  data = csv.reader(f)
 	  for row in data:
-	        print(row)
+	        print(row, len(row))
 	return rows
 
 
 if __name__ == "__main__":
 	d_rows = load('data/douban.csv')
+
 	print('-'*30)
 	w_rows = load('data/weibo.csv')
