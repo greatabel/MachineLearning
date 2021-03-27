895621f552df6a23d895f7bba2c12fba8d898c16
greatabel <myreceiver2for2github@gmail.com>
2021-03-28 04:07:33 +0800

    compare begin

--- a10109account_relevance/i2compare.py
+++ a10109account_relevance/i2compare.py
@@ -1,4 +1,7 @@
 import csv
+import jellyfish
+from termcolor import colored
+
 
 def load(filepath):
 	rows = []
@@ -6,11 +9,23 @@ def load(filepath):
 	  data = csv.reader(f)
 	  for row in data:
 	        print(row, len(row))
+	        rows.append(row)
 	return rows
 
 
+def compare(source_record, target_dataset):
+	print(source_record[0],source_record[1])
+
+	c0 = jellyfish.levenshtein_distance(u'jellyfish', u'smellyfish')
+	c1 = jellyfish.jaro_distance(u'jellyfish', u'smellyfish')
+	c2 = jellyfish.damerau_levenshtein_distance(u'jellyfish', u'jellyfihs')
+	print(c0, c1, c2)
+	
 if __name__ == "__main__":
 	d_rows = load('data/douban.csv')
 
-	print('-'*30)
+	print(colored('数据集分割线'+'-'*30, 'red'))
 	w_rows = load('data/weibo.csv')
+	print(colored('compare'+'-'*30, 'red'))
+	# 例子
+	compare(d_rows[0], w_rows)
