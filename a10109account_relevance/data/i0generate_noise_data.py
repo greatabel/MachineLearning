e027e622e0fad3c10693bb8076045bdbfeff7835
greatabel <myreceiver2for2github@gmail.com>
2021-03-28 04:26:07 +0800

    name bio compare

--- a10109account_relevance/data/i0generate_noise_data.py
+++ a10109account_relevance/data/i0generate_noise_data.py
@@ -7,11 +7,12 @@ res_lit = []
 #生成100个
 for i in range(24):
     #从大字符串中随机生成8位数的字符，换行并转换成字符串
+	res0  =  ''.join(random.sample(all_str,4))
 	res  =  ''.join(random.sample(all_str,8))
 	bio  = 'noise data '+ ''.join(random.sample(all_str,30))
 	if res not in res_lit:
 		if i > 3:
-			print(str(i)+","+res+",'上海','"+bio+"'")
+			print(str(i)+","+ res0+","+res+",'上海','"+bio+"'")
 		res_lit.append((i,res))
 
 
@@ -19,9 +20,10 @@ print('-'*30)
 
 for i in range(24):
     #从大字符串中随机生成8位数的字符，换行并转换成字符串
+	res0  =  ''.join(random.sample(all_str,5))
 	res  =  ''.join(random.choice("0123456789") for i in range(16))
 	bio  = 'noise data '+ ''.join(random.sample(all_str,30))
 	if res not in res_lit:
 		if i > 3:
-			print(str(i)+","+res+",'上海','"+bio+"'")
+			print(str(i)+","+res0+","+res+",'上海','"+bio+"'")
 		res_lit.append((i,res))
--- a10109account_relevance/i2compare.py
+++ a10109account_relevance/i2compare.py
@@ -2,6 +2,7 @@ import csv
 import jellyfish
 from termcolor import colored
 
+# https://towardsdatascience.com/calculating-string-similarity-in-python-276e18a7d33a
 
 def load(filepath):
 	rows = []
@@ -13,14 +14,20 @@ def load(filepath):
 	return rows
 
 
-def compare(source_record, target_dataset):
-	print(source_record[0],source_record[1])
+def compare(source_record, target_list):
+	sname = source_record[2]
+	s_bio = source_record[4]
+	print(sname, s_bio, '#'*10)
+	for i in range(0, len(target_list)):
+		target = target_list[i]
+		tname = target[2]
+		t_bio = target[4]
+		c0 = jellyfish.levenshtein_distance(sname, tname)
+		c1 = jellyfish.jaro_distance(sname, tname)
+		c2 = jellyfish.damerau_levenshtein_distance(sname, tname)
+		b0 = jellyfish.jaro_distance(s_bio, t_bio)
+		print('target index=', i, 'name silimarity=', c0, c1, c2, 'bio silimarity=', b0)
 
-	c0 = jellyfish.levenshtein_distance(u'jellyfish', u'smellyfish')
-	c1 = jellyfish.jaro_distance(u'jellyfish', u'smellyfish')
-	c2 = jellyfish.damerau_levenshtein_distance(u'jellyfish', u'jellyfihs')
-	print(c0, c1, c2)
-	
 if __name__ == "__main__":
 	d_rows = load('data/douban.csv')
 
