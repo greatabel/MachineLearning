5821060d9d2f26ca9ef2f56f1307b56cd8df4923
greatabel <myreceiver2for2github@gmail.com>
2021-08-16 13:57:33 +0800

    init of b13854

---
+++ b13854_NLP/i0prepare_small_dataset.py
@@ -0,0 +1,26 @@
+import json
+
+with open('data/simplified-nq-test.jsonl', 'r') as json_file:
+    json_list = list(json_file)
+
+# print('@'*20, len(json_list))
+length = len(json_list)
+
+# for json_str in json_list[0:1]:
+#     result = json.loads(json_str)
+#     print(f"result: {result}")
+#     print(isinstance(result, dict))
+
+
+with open('data/mytrain.jsonl', 'w') as outfile:
+	for json_str in json_list[0:length//2]:
+		result = json.loads(json_str)
+		json.dump(result, outfile)
+		outfile.write('\n')
+
+
+with open('data/mytest.jsonl', 'w') as outfile:
+	for json_str in json_list[length//2:]:
+		result = json.loads(json_str)
+		json.dump(result, outfile)
+		outfile.write('\n')
