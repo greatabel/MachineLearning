b6ffb74772361af9290e398100ef2b466dce4824
greatabel <myreceiver2for2github@gmail.com>
2021-11-13 23:51:40 +0800

    format py

--- b13392_travel_route_planning/i4openstreetmap_main.py
+++ b13392_travel_route_planning/i4openstreetmap_main.py
@@ -19,6 +19,7 @@ from flask import Flask, Response
 from flask import jsonify
 from flask_cors import CORS
 from movie import create_app
+
 # from flask import Flask
 
 import folium
@@ -26,7 +27,7 @@ from folium import plugins
 import numpy as np
 from weather import wheather_R
 
-print('#'*20, wheather_R)
+print("#" * 20, wheather_R)
 app = create_app()
 app.secret_key = "ABCabc123"
 app.debug = True
@@ -43,11 +44,9 @@ db = SQLAlchemy(app)
 # --- end   数据库 ---
 admin_list = ["admin@126.com"]
 
+
 def folim_create(start_coords, orginal=True):
-    m = folium.Map(location=start_coords, width='100%', height='70%', zoom_start=14)
-
-
-
+    m = folium.Map(location=start_coords, width="100%", height="70%", zoom_start=14)
 
     # line_to_new_delhi = folium.PolyLine(
     #     [
@@ -58,53 +57,98 @@ def folim_create(start_coords, orginal=True):
     #         [28.65203063, 76.81640625],
     #     ]
     # ).add_to(m)
-    tooltip ='click me to see more'
+    tooltip = "click me to see more"
 
-    ic0 = plugins.BeautifyIcon(border_color='#00ABDC',
-                           text_color='#00ABDC',
-                           number=0,
-                           inner_icon_style='margin-top:0;')
-    ic1 = plugins.BeautifyIcon(border_color='#00ABDC',
-                           text_color='#00ABDC',
-                           number=1,
-                           inner_icon_style='margin-top:0;')
-    ic2 = plugins.BeautifyIcon(border_color='#00ABDC',
-                           text_color='#00ABDC',
-                           number=2,
-                           inner_icon_style='margin-top:0;')
-    ic3 = plugins.BeautifyIcon(border_color='#00ABDC',
-                           text_color='#00ABDC',
-                           number=3,
-                           inner_icon_style='margin-top:0;')
-    ic4 = plugins.BeautifyIcon(border_color='#00ABDC',
-                           text_color='#00ABDC',
-                           number=4,
-                           inner_icon_style='margin-top:0;')
+    ic0 = plugins.BeautifyIcon(
+        border_color="#00ABDC",
+        text_color="#00ABDC",
+        number=0,
+        inner_icon_style="margin-top:0;",
+    )
+    ic1 = plugins.BeautifyIcon(
+        border_color="#00ABDC",
+        text_color="#00ABDC",
+        number=1,
+        inner_icon_style="margin-top:0;",
+    )
+    ic2 = plugins.BeautifyIcon(
+        border_color="#00ABDC",
+        text_color="#00ABDC",
+        number=2,
+        inner_icon_style="margin-top:0;",
+    )
+    ic3 = plugins.BeautifyIcon(
+        border_color="#00ABDC",
+        text_color="#00ABDC",
+        number=3,
+        inner_icon_style="margin-top:0;",
+    )
+    ic4 = plugins.BeautifyIcon(
+        border_color="#00ABDC",
+        text_color="#00ABDC",
+        number=4,
+        inner_icon_style="margin-top:0;",
+    )
 
     if orginal != True:
         # folium.Marker([51.5205898, -0.1424225], popup='ondon Central Hostel',tooltip=tooltip,icon=folium.Icon(color='red')).add_to(m)
-        folium.Marker([51.5205898, -0.1424225],popup='London Central Hostel',tooltip=tooltip, icon=ic0).add_to(m)
-        folium.Marker([51.503324, -0.119543], popup='Coca-Cola London Eye',tooltip=tooltip,icon=ic1).add_to(m)
-        folium.Marker([51.5138453, -0.0983506], popup="St. Paul's Cathedral",tooltip=tooltip,icon=ic2).add_to(m)
-        folium.Marker([51.5128019, -0.0834833], popup='Leadenhall Market',tooltip=tooltip,icon=ic3).add_to(m)
-        folium.Marker([51.508929 , -0.128299], popup='The National Gallery',tooltip=tooltip,icon=ic4).add_to(m)
+        folium.Marker(
+            [51.5205898, -0.1424225],
+            popup="London Central Hostel",
+            tooltip=tooltip,
+            icon=ic0,
+        ).add_to(m)
+        folium.Marker(
+            [51.503324, -0.119543],
+            popup="Coca-Cola London Eye",
+            tooltip=tooltip,
+            icon=ic1,
+        ).add_to(m)
+        folium.Marker(
+            [51.5138453, -0.0983506],
+            popup="St. Paul's Cathedral",
+            tooltip=tooltip,
+            icon=ic2,
+        ).add_to(m)
+        folium.Marker(
+            [51.5128019, -0.0834833],
+            popup="Leadenhall Market",
+            tooltip=tooltip,
+            icon=ic3,
+        ).add_to(m)
+        folium.Marker(
+            [51.508929, -0.128299],
+            popup="The National Gallery",
+            tooltip=tooltip,
+            icon=ic4,
+        ).add_to(m)
         line_to_new_delhi = folium.PolyLine(
             [
                 [51.5205898, -0.1424225],
                 [51.503324, -0.119543],
                 [51.5138453, -0.0983506],
                 [51.5128019, -0.0834833],
-                [51.508929 , -0.128299],
+                [51.508929, -0.128299],
             ],
-            color='red'
+            color="red",
         ).add_to(m)
         plugins.PolyLineTextPath(line_to_new_delhi, "London Tour", offset=-25).add_to(m)
     else:
-        folium.Marker([51.5205898, -0.1424225],popup='London Central Hostel',tooltip=tooltip).add_to(m)
-        folium.Marker([51.503324, -0.119543], popup='Coca-Cola London Eye',tooltip=tooltip).add_to(m)
-        folium.Marker([51.5138453, -0.0983506], popup="St. Paul's Cathedral",tooltip=tooltip).add_to(m)
-        folium.Marker([51.5128019, -0.0834833], popup='Leadenhall Market',tooltip=tooltip).add_to(m)
-        folium.Marker([51.508929 , -0.128299], popup='The National Gallery',tooltip=tooltip).add_to(m)
+        folium.Marker(
+            [51.5205898, -0.1424225], popup="London Central Hostel", tooltip=tooltip
+        ).add_to(m)
+        folium.Marker(
+            [51.503324, -0.119543], popup="Coca-Cola London Eye", tooltip=tooltip
+        ).add_to(m)
+        folium.Marker(
+            [51.5138453, -0.0983506], popup="St. Paul's Cathedral", tooltip=tooltip
+        ).add_to(m)
+        folium.Marker(
+            [51.5128019, -0.0834833], popup="Leadenhall Market", tooltip=tooltip
+        ).add_to(m)
+        folium.Marker(
+            [51.508929, -0.128299], popup="The National Gallery", tooltip=tooltip
+        ).add_to(m)
     # line_to_hanoi = folium.PolyLine(
     #     [
     #         [28.76765911, 77.60742188],
@@ -114,58 +158,53 @@ def folim_create(start_coords, orginal=True):
     #     ]
     # ).add_to(m)
 
-
-    
-
-
     # plugins.PolyLineTextPath(line_to_hanoi, "To Hanoi", offset=-5).add_to(m)
     return m
 
-'''
+
+"""
 get from i3data_visualization
 
-'''
-fever_dict = {
-    0: 3,
-    1: 640,
-    2: 322,
-    3: 52,
-    4: 1607
-}
-'''
+"""
+fever_dict = {0: 3, 1: 640, 2: 322, 3: 52, 4: 1607}
+"""
 get from i3data_visualization and i2sentimanet_analys
 
-'''
+"""
 sentiment_dict = {
     0: [3, 0, 0],
     1: [202, 404, 101],
     2: [0, 303, 101],
     3: [0, 101, 0],
-    4: [808, 606, 202]
+    4: [808, 606, 202],
 }
 
 additonal_information = [fever_dict, sentiment_dict]
 
+
 def my_haversine_distance(lat1, lon1, lat2, lon2):
-   r = 6371
-   phi1 = np.radians(lat1)
-   phi2 = np.radians(lat2)
-   delta_phi = np.radians(lat2 - lat1)
-   delta_lambda = np.radians(lon2 - lon1)
-   a = np.sin(delta_phi / 2)**2 + np.cos(phi1) * np.cos(phi2) *  np.sin(delta_lambda / 2)**2
-   res = r * (2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a)))
-   return np.round(res, 2)
+    r = 6371
+    phi1 = np.radians(lat1)
+    phi2 = np.radians(lat2)
+    delta_phi = np.radians(lat2 - lat1)
+    delta_lambda = np.radians(lon2 - lon1)
+    a = (
+        np.sin(delta_phi / 2) ** 2
+        + np.cos(phi1) * np.cos(phi2) * np.sin(delta_lambda / 2) ** 2
+    )
+    res = r * (2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a)))
+    return np.round(res, 2)
 
 
 def hotness_sentiment_improve(base, vote):
-    M = math.e*base*100
+    M = math.e * base * 100
     upper = 50
     down = 30
-    a = M + math.log(math.e **(vote/2)) 
+    a = M + math.log(math.e ** (vote / 2))
     b = M + math.log(math.e ** 1)
 
-    r = a *1.5/ b
-    print('hotness_sentiment_improve a,b=',a, b, 'result=', r)
+    r = a * 1.5 / b
+    print("hotness_sentiment_improve a,b=", a, b, "result=", r)
     return r
 
 
@@ -175,25 +214,30 @@ def advanced_folim_create(start_coords, additonal_information=additonal_informat
     sentiment_dict = additonal_information[1]
 
     total_km = []
-    
+
     lat_logs = [
-                [51.5205898, -0.1424225],
-                [51.503324, -0.119543],
-                [51.5138453, -0.0983506],
-                [51.5128019, -0.0834833],
-                [51.508929 , -0.128299],
-            ]
-    places = ['YHA London Central Hostel', 'Coca-Cola London Eye', 
-            'St. Paul\'s Cathedral', 'Leadenhall Market', 'The National Gallery']
-    if not 'rain' in wheather_R and not 'snow' in wheather_R:
+        [51.5205898, -0.1424225],
+        [51.503324, -0.119543],
+        [51.5138453, -0.0983506],
+        [51.5128019, -0.0834833],
+        [51.508929, -0.128299],
+    ]
+    places = [
+        "YHA London Central Hostel",
+        "Coca-Cola London Eye",
+        "St. Paul's Cathedral",
+        "Leadenhall Market",
+        "The National Gallery",
+    ]
+    if not "rain" in wheather_R and not "snow" in wheather_R:
         for source in lat_logs:
             distances_km = []
             for row in lat_logs:
-               distances_km.append(
-                   my_haversine_distance(source[0], source[1], row[0], row[1])
-               )
+                distances_km.append(
+                    my_haversine_distance(source[0], source[1], row[0], row[1])
+                )
             total_km.append(distances_km)
-        print('total_km=', total_km)
+        print("total_km=", total_km)
         distances_sum = []
         for idx, val in enumerate(total_km):
             print(idx, sum(val))
@@ -206,17 +250,14 @@ def advanced_folim_create(start_coords, additonal_information=additonal_informat
             p = distances_sum[i] * r
             new_values.append(p)
 
-        print('#'*20, new_values)
+        print("#" * 20, new_values)
         orders = np.argsort(new_values)
-        print('order=', orders)
+        print("order=", orders)
         sorted_places = np.array(lat_logs)[orders]
         sorted_placenames = np.array(places)[orders]
-        print('sorted_places=', sorted_places)
-        print('sorted_placenames=', sorted_placenames)
-        m = folium.Map(location=start_coords, width='100%', height='70%', zoom_start=14)
-
-
-
+        print("sorted_places=", sorted_places)
+        print("sorted_placenames=", sorted_placenames)
+        m = folium.Map(location=start_coords, width="100%", height="70%", zoom_start=14)
 
         # line_to_new_delhi = folium.PolyLine(
         #     [
@@ -227,47 +268,58 @@ def advanced_folim_create(start_coords, additonal_information=additonal_informat
         #         [28.65203063, 76.81640625],
         #     ]
         # ).add_to(m)
-        tooltip ='click me to see more'
+        tooltip = "click me to see more"
 
-        ic0 = plugins.BeautifyIcon(border_color='#00ABDC',
-                               text_color='#00ABDC',
-                               number=0,
-                               inner_icon_style='margin-top:0;')
-        ic1 = plugins.BeautifyIcon(border_color='#00ABDC',
-                               text_color='#00ABDC',
-                               number=1,
-                               inner_icon_style='margin-top:0;')
-        ic2 = plugins.BeautifyIcon(border_color='#00ABDC',
-                               text_color='#00ABDC',
-                               number=2,
-                               inner_icon_style='margin-top:0;')
-        ic3 = plugins.BeautifyIcon(border_color='#00ABDC',
-                               text_color='#00ABDC',
-                               number=3,
-                               inner_icon_style='margin-top:0;')
-        ic4 = plugins.BeautifyIcon(border_color='#00ABDC',
-                               text_color='#00ABDC',
-                               number=4,
-                               inner_icon_style='margin-top:0;')
+        ic0 = plugins.BeautifyIcon(
+            border_color="#00ABDC",
+            text_color="#00ABDC",
+            number=0,
+            inner_icon_style="margin-top:0;",
+        )
+        ic1 = plugins.BeautifyIcon(
+            border_color="#00ABDC",
+            text_color="#00ABDC",
+            number=1,
+            inner_icon_style="margin-top:0;",
+        )
+        ic2 = plugins.BeautifyIcon(
+            border_color="#00ABDC",
+            text_color="#00ABDC",
+            number=2,
+            inner_icon_style="margin-top:0;",
+        )
+        ic3 = plugins.BeautifyIcon(
+            border_color="#00ABDC",
+            text_color="#00ABDC",
+            number=3,
+            inner_icon_style="margin-top:0;",
+        )
+        ic4 = plugins.BeautifyIcon(
+            border_color="#00ABDC",
+            text_color="#00ABDC",
+            number=4,
+            inner_icon_style="margin-top:0;",
+        )
 
         index = 0
         for p, name in zip(sorted_places, sorted_placenames):
-            print(p[0], p[1], '#'*10, name)
-            myic = plugins.BeautifyIcon(border_color='#00ABDC',
-                               text_color='#00ABDC',
-                               number=index,
-                               inner_icon_style='margin-top:0;')
+            print(p[0], p[1], "#" * 10, name)
+            myic = plugins.BeautifyIcon(
+                border_color="#00ABDC",
+                text_color="#00ABDC",
+                number=index,
+                inner_icon_style="margin-top:0;",
+            )
             # folium.Marker([51.5205898, -0.1424225], popup='ondon Central Hostel',tooltip=tooltip,icon=folium.Icon(color='red')).add_to(m)
-            folium.Marker([p[0], p[1]], popup=name,tooltip=tooltip, icon=myic).add_to(m)
+            folium.Marker([p[0], p[1]], popup=name, tooltip=tooltip, icon=myic).add_to(
+                m
+            )
             index += 1
         # folium.Marker([51.503324, -0.119543], popup='Coca-Cola London Eye',tooltip=tooltip,icon=ic1).add_to(m)
         # folium.Marker([51.5138453, -0.0983506], popup="St. Paul's Cathedral",tooltip=tooltip,icon=ic2).add_to(m)
         # folium.Marker([51.5128019, -0.0834833], popup='Leadenhall Market',tooltip=tooltip,icon=ic3).add_to(m)
         # folium.Marker([51.508929 , -0.128299], popup='The National Gallery',tooltip=tooltip,icon=ic4).add_to(m)
-        line_to_new_delhi = folium.PolyLine(
-            sorted_places,
-            color='red'
-        ).add_to(m)
+        line_to_new_delhi = folium.PolyLine(sorted_places, color="red").add_to(m)
         plugins.PolyLineTextPath(line_to_new_delhi, "London Tour", offset=-25).add_to(m)
 
         # line_to_hanoi = folium.PolyLine(
@@ -279,15 +331,11 @@ def advanced_folim_create(start_coords, additonal_information=additonal_informat
         #     ]
         # ).add_to(m)
 
-
-        
-
-
         # plugins.PolyLineTextPath(line_to_hanoi, "To Hanoi", offset=-5).add_to(m)
         return m
 
 
-@app.route('/orginal')
+@app.route("/orginal")
 def orginal():
     start_coords = (51.503324, -0.119543)
     # folium_map = folium.Map(location=start_coords, zoom_start=14)
@@ -295,21 +343,20 @@ def orginal():
     return folium_map._repr_html_()
 
 
-
-@app.route('/path')
+@app.route("/path")
 def index():
     start_coords = (51.503324, -0.119543)
     # folium_map = folium.Map(location=start_coords, zoom_start=14)
     folium_map = folim_create(start_coords, orginal=False)
     return folium_map._repr_html_()
 
-@app.route('/advanced_path')
+
+@app.route("/advanced_path")
 def advance_path():
     start_coords = (51.503324, -0.119543)
     # folium_map = folium.Map(location=start_coords, zoom_start=14)
     folium_map = advanced_folim_create(start_coords, additonal_information)
     return folium_map._repr_html_()
-
 
 
 class User(db.Model):
@@ -677,9 +724,6 @@ def logout():
 
 
 reviews = []
-
-
-
 
 
 @login_manager.unauthorized_handler
