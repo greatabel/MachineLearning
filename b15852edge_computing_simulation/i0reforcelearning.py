de4df80e120148642d9f5cc810ab38b87f2cb79d
greatabel <myreceiver2for2github@gmail.com>
2021-12-21 09:35:58 +0800

    i1heuristic_algorithm.py

--- b15852edge_computing_simulation/i0reforcelearning.py
+++ b15852edge_computing_simulation/i0reforcelearning.py
@@ -1,19 +1,26 @@
 import gym
+import numpy as np
+np.random.seed(0)
+import pandas as pd
 
 # https://stackoverflow.com/questions/40195740/how-to-run-openai-gym-render-over-a-server/44426542#44426542
 from IPython import display
 import matplotlib.pyplot as plt
 import time
 
+from i1heuristic_algorithm import SARSAAgent, play_sarsa
 
+
+print('-'*20, 'step 0')
 env = gym.make("Taxi-v3")
 env.seed(0)
+print('建立负载映射环境')
 print('观测空间 = {}'.format(env.observation_space))
 print('动作空间 = {}'.format(env.action_space))
 print('状态数量 = {}, 动作数量 = {}'.format(env.nS, env.nA))
 
 env.reset()
-for s in range(10):
+for s in range(3):
     print("step=", s)
     ####
     # plt.imshow(env.render(mode="rgb_array"))
@@ -25,3 +32,38 @@ for s in range(10):
     env.step(action)
 
 env.close()
+
+print('-'*20, 'step 1')
+state = env.reset()
+taxirow, taxicol, passloc, destidx = env.unwrapped.decode(state)
+print(taxirow, taxicol, passloc, destidx)
+print('模拟运载程序块位置 = {}'.format((taxirow, taxicol)))
+print('模拟访问和计算程序内存位置 = {}'.format(env.unwrapped.locs[passloc]))
+print('目标位置 = {}'.format(env.unwrapped.locs[destidx]))
+env.render()
+
+print('-'*20, 'step 2')
+env.step(0)
+env.render()
+
+print('-'*20, 'step 3')
+print('启发式算法模拟')
+agent = SARSAAgent(env)
+
+# 训练
+episodes = 300
+episode_rewards = []
+for episode in range(episodes):
+    episode_reward = play_sarsa(env, agent, train=True)
+    episode_rewards.append(episode_reward)
+    
+plt.plot(episode_rewards)
+plt.show()
+
+# 测试
+agent.epsilon = 0. # 取消探索
+
+episode_rewards = [play_sarsa(env, agent) for _ in range(100)]
+print('平均回合奖励 = {} / {} = {}'.format(sum(episode_rewards),
+        len(episode_rewards), np.mean(episode_rewards)))
+
