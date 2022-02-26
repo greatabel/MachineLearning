ab331fc5d0416da8026dc13dd0e0cebbb883323d
greatabel <myreceiver2for2github@gmail.com>
2022-02-26 23:01:27 +0800

    mv a17694_deeprm_tf

--- b17616finger_classify/i0image_preprocessing.py
+++ b17616finger_classify/i0image_preprocessing.py
@@ -1,6 +1,7 @@
 import os
 
 
-for filename in os.listdir("orignal_data/n/1/指背"):             
+path = "orignal_data/n/1/指背"
+for filename in os.listdir(path):             
 	#listdir的参数是文件夹的路径
     print(filename)     
