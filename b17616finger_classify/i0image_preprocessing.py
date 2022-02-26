f33db45a187ba532ca9e6e4f5693fd9ea478ddfd
greatabel <myreceiver2for2github@gmail.com>
2022-02-26 23:37:23 +0800

    i0 change

--- b17616finger_classify/i0image_preprocessing.py
+++ b17616finger_classify/i0image_preprocessing.py
@@ -1,7 +1,19 @@
 import os
+import cv2
 
 
-path = "orignal_data/n/1/指背"
-for filename in os.listdir(path):             
+
+path_n = "orignal_data/n/1/指背"
+path_s = "orignal_data/s/1/指背"
+
+for filename in os.listdir(path_n):             
 	#listdir的参数是文件夹的路径
-    print(filename)     
+    print(filename)
+    file_path =  path_n + '/' +filename
+    # print(file_path)
+    im = cv2.imread(file_path)
+    # 截取需要的部分，排除干扰
+    im_0 = im[140:380,0:640]
+    print(im.shape, '#', im_0.shape)
+    cv2.imshow('t', im_0)
+    cv2.waitKey()
