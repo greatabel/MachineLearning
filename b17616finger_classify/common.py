e4dc56770f0e5d6f8ccacd319d57661bf74a7df3
greatabel <myreceiver2for2github@gmail.com>
2022-02-27 01:17:03 +0800

    i1 & i2

---
+++ b17616finger_classify/common.py
@@ -0,0 +1,23 @@
+import os
+import cv2
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+labels = ["n", "s"]
+
+def get_training_data(data_dir):
+    data = list()
+    for label in labels:
+        path = os.path.join(data_dir, label)
+        class_num = labels.index(label)
+        for img in os.listdir(path):
+            try:
+                img_arr = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+                # resized_arr = cv2.resize(img_arr, (img_size, img_size))
+                data.append([img_arr, class_num])
+                # data.append([resized_arr, class_num])
+            except Exception as e:
+                print(e)
+    return np.array(data)
--- b17616finger_classify/i0image_preprocessing.py
+++ b17616finger_classify/i0image_preprocessing.py
@@ -2,18 +2,27 @@ import os
 import cv2
 
 
-
-path_n = "orignal_data/n/1/指背"
+# path_n = "orignal_data/n/1/指背"
 path_s = "orignal_data/s/1/指背"
 
-for filename in os.listdir(path_n):             
-	#listdir的参数是文件夹的路径
+# save_path = "data/n"
+save_path = "data/s"
+
+for filename in os.listdir(path_s):
+    # for filename in os.listdir(path_n):
+    # listdir的参数是文件夹的路径
     print(filename)
-    file_path =  path_n + '/' +filename
+    file_path = path_s + "/" + filename
+    # file_path =  path_n + '/' +filename
     # print(file_path)
     im = cv2.imread(file_path)
-    # 截取需要的部分，排除干扰
-    im_0 = im[140:380,0:640]
-    print(im.shape, '#', im_0.shape)
-    cv2.imshow('t', im_0)
+    # 截取需要的部分，排除干扰，去掉指甲部分，去掉背景黑色部分，只留下静脉区
+    im_0 = im[180:310, 0:600]
+    print(im.shape, "#", im_0.shape)
+
+    # 保存
+    save_path_file = os.path.join(save_path, filename)
+    # save_path_file = os.path.join(save_path,filename+'.jpg')
+    cv2.imwrite(save_path_file, im_0)
+    cv2.imshow("t", im_0)
     cv2.waitKey()
