b7b19f3a7d5e2e384a8663f998d39f6b94f9dbd6
greatabel <myreceiver2for2github@gmail.com>
2022-03-14 15:12:29 +0800

    init of b18619_b15020pclean

---
+++ b18619_b15020pclean/i1data_hospital_explore.py
@@ -0,0 +1,20 @@
+import numpy as np  # linear algebra
+
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import seaborn as sns
+import matplotlib.pyplot as plt
+from IPython.display import display
+import json
+
+
+print("---- 1. let us check the first document in the dataset ----")
+df = pd.read_csv("datasets/hospital_dirty.csv")
+print(df.head())
+
+
+doc_text_words = df["HospitalName"].apply(lambda x: len(x.split(" ")))
+plt.figure(figsize=(12, 6))
+sns.distplot(doc_text_words.values, kde=True, hist=False).set_title(
+    "Distribution of text word count of all HospitalName"
+)
+plt.savefig("reports/i2.png")
---
+++ b18619_b15020pclean/i4prior_knowledge.py
@@ -0,0 +1,6 @@
+import json
+
+from libpgm.nodedata import NodeData
+from libpgm.graphskeleton import GraphSkeleton
+from libpgm.discretebayesiannetwork import DiscreteBayesianNetwork
+from libpgm.pgmlearner import PGMLearner
