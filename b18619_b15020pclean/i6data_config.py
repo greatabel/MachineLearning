917c7f7388c5ee834679da327805dcf8da24713e
greatabel <myreceiver2for2github@gmail.com>
2022-05-02 10:30:17 +0800

    b18805

--- b18619_b15020pclean/i6data_config.py
+++
@@ -1,6 +0,0 @@
-header = [('location', 'cost'),
-          ('quality', 'cost'),
-          ('cost', 'no_of_people'),
-          ('location', 'no_of_people')]
-location_distribute = [[0.6], [0.4]]
-quality_distribute = [[0.3], [0.5], [0.2]]
