266c0afd56d4c6615a1f3faa677a29f046063e44
greatabel <myreceiver2for2github@gmail.com>
2022-09-06 22:59:01 +0800

    visual

---
+++ a22005_sentiment_with_Apk/i4security_predict_web/movie/static/i3data_visualization/00.js
@@ -0,0 +1,4 @@
+var DATA = {
+    'data2021':14000, 'data2020': 15,'total_sentiment_polarity':2.8, 'average_sentiment_polarity':0.56,'num_positive':2022, 'num_neural':10080,'num_nagtive':1022, 'data2021':99 };
+var RELATED_WORDS = {'China':12,'South China Sea': 5, '发展':3 };
+var WHO_TWEETS = {'Tek 🕷🐐🐗🏳️‍⚧️':2,'べき':1,'こころ＠そのまま＃相互':1,'阿井幸作':1, };
