d7491e72c38ff0321a1f9dc8c7e0cb9ae0543ed2
greatabel <myreceiver2for2github@gmail.com>
2023-02-21 19:37:31 +0800

    with file

--- a25321_melody_extraction/i2polyphonic_test.py
+++ a25321_melody_extraction/i2polyphonic_test.py
@@ -5,6 +5,9 @@ import numpy as np
 import librosa
 import mido
 
+import timeit
+
+
 # Open the WAV file for reading
 with wave.open("vocals.wav", "rb") as wav_file:
     # Get the parameters of the WAV file
@@ -31,55 +34,86 @@ with wave.open("i2polyphonic.wav", "wb") as new_wav_file:
     # Write the modified data to the new WAV file
     new_wav_file.writeframes(new_wav_data)
 
+
 # 如果需要变成midi格式，打开下面的librosa代码
 # 默认关闭了，为了效率
 
-# # Load audio file
-# filename = 'i2polyphonic.wav'
-# y, sr = librosa.load(filename)
+print('part II')
+starttime = timeit.default_timer()
 
-# # Get chroma features
-# chroma = librosa.feature.chroma_stft(y=y, sr=sr)
+# Load audio file
+filename = 'i2polyphonic.wav'
+y, sr = librosa.load(filename)
 
-# # Normalize chroma features
-# chroma_norm = librosa.util.normalize(chroma)
+# Get chroma features
+chroma = librosa.feature.chroma_stft(y=y, sr=sr)
 
-# # Create MIDI file
-# midi = mido.MidiFile()
-# track = mido.MidiTrack()
-# midi.tracks.append(track)
+# Normalize chroma features
+chroma_norm = librosa.util.normalize(chroma)
 
-# # Set tempo
-# bpm = 120
-# mpqn = mido.bpm2tempo(bpm)
-# track.append(mido.MetaMessage('set_tempo', tempo=mpqn))
+# Create MIDI file
+midi = mido.MidiFile()
+track = mido.MidiTrack()
+midi.tracks.append(track)
 
-# # Set time signature
-# time_signature = (4, 4)
-# clocks_per_click = 24
-# notated_32nd_notes_per_beat = 8
-# track.append(mido.MetaMessage('time_signature', numerator=time_signature[0], denominator=time_signature[1],
-#                               clocks_per_click=clocks_per_click, notated_32nd_notes_per_beat=notated_32nd_notes_per_beat))
+# Set tempo
+bpm = 120
+mpqn = mido.bpm2tempo(bpm)
+track.append(mido.MetaMessage('set_tempo', tempo=mpqn))
 
-# # Add note events
-# note_on_time = 0
-# note_off_time = 0
-# velocity = 64
-# for frame in range(chroma_norm.shape[1]):
-#     # Get pitch values from chroma features
-#     pitches = np.where(chroma_norm[:, frame] > 0.5)[0] + 24
-#     if len(pitches) > 0:
-#         # Note on
-#         note_on_time = int(frame * 512 / sr)  # Convert frame index to time in ticks
-#         for pitch in pitches:
-#             track.append(mido.Message('note_on', note=pitch, velocity=velocity, time=0))
-#         # Note off
-#         note_off_time = int((frame+1) * 512 / sr)  # Convert frame index to time in ticks
-#         for pitch in pitches:
-#             track.append(mido.Message('note_off', note=pitch, velocity=velocity, time=note_off_time - note_on_time))
+# Set time signature
+time_signature = (4, 4)
+clocks_per_click = 24
+notated_32nd_notes_per_beat = 8
+track.append(mido.MetaMessage('time_signature', numerator=time_signature[0], denominator=time_signature[1],
+                              clocks_per_click=clocks_per_click, notated_32nd_notes_per_beat=notated_32nd_notes_per_beat))
 
-# # Save MIDI file
-# midi_filename = 'i2polyphonic.mid'
+# Add note events
+note_on_time = 0
+note_off_time = 0
+velocity = 64
+for frame in range(chroma_norm.shape[1]):
+    # Get pitch values from chroma features
+    pitches = np.where(chroma_norm[:, frame] > 0.5)[0] + 24
+    if len(pitches) > 0:
+        # Note on
+        note_on_time = int(frame * 512 / sr)  # Convert frame index to time in ticks
+        for pitch in pitches:
+            track.append(mido.Message('note_on', note=pitch, velocity=velocity, time=0))
+        # Note off
+        note_off_time = int((frame+1) * 512 / sr)  # Convert frame index to time in ticks
+        for pitch in pitches:
+            track.append(mido.Message('note_off', note=pitch, velocity=velocity, time=note_off_time - note_on_time))
+
+# Save MIDI file
+midi_filename = 'i2polyphonic.mid'
 # midi.save(midi_filename)
 
+print("The time difference is :", timeit.default_timer() - starttime)
 
+'''
+The time difference is : 0.4281404999999978
+
+The time difference is : 0.4072299999999984
+
+The time difference is : 0.392059291999999
+'''
+
+from matplotlib import pyplot as plt
+
+
+
+
+names = ['1 time', '2 time', '3 time']
+values = [0.42, 0.40, 0.39]
+
+plt.figure(figsize=(9, 3))
+
+plt.subplot(131)
+plt.bar(names, values)
+plt.subplot(132)
+plt.scatter(names, values)
+plt.subplot(133)
+plt.plot(names, values)
+plt.suptitle('Conversion speed  Compare')
+plt.show()
