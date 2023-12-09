c875b70576438fc41bcef2c2883f3c7af84dcecd
greatabel <myreceiver2for2github@gmail.com>
2023-12-09 15:16:22 +0800

    ch04

--- Huggingface_Book/ch04index.py
+++ Huggingface_Book/ch04index.py
@@ -4,3 +4,19 @@ from datasets import list_metrics
 metrics_list = list_metrics()
 
 print(	len(metrics_list), metrics_list[:5] )
+print('-'*20)
+
+from datasets import load_metric
+
+metric = load_metric(path='glue', config_name='mrpc')
+
+print(metric.inputs_description)
+
+#第4章/计算一个评价指标
+predictions = [0, 1, 0]
+references = [0, 1, 1]
+
+m = metric.compute(predictions=predictions, references=references)
+
+print('-'*20)
+print(m)
