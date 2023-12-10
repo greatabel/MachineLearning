5ff7699f39984697870b99bced89fcdfd21dcd72
greatabel <myreceiver2for2github@gmail.com>
2023-12-10 21:56:59 +0800

    fix error

--- ReinforcementLearning_Theory&PyImplementation/ch02markov/i6cliff_walking_completed.py
+++ ReinforcementLearning_Theory&PyImplementation/ch02markov/i6cliff_walking_completed.py
@@ -7,7 +7,7 @@ import time
 
 
 env = gym.make("CliffWalking-v0")
-env.seed(0)
+# env.seed(0)
 print('观测空间 = {}'.format(env.observation_space))
 print('动作空间 = {}'.format(env.action_space))
 print('状态数量 = {}, 动作数量 = {}'.format(env.nS, env.nA))
